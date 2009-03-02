eb860ff208fb9cea42f6305d8ce45f74e2491fcd
minahito <minahito@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-03-02 16:46:13 +0000

    Fix bug #2491817
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@70 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/legacy/admin/actions/SmilesEditAction.class.php
+++ html/modules/legacy/admin/actions/SmilesEditAction.class.php
@@ -44,8 +44,8 @@ class Legacy_SmilesEditAction extends Legacy_AbstractEditAction
 		//
 		// Delete old file, if the file exists.
 		//
-		if ($this->mActionForm->mOldAvatarFilename != null && $this->mActionForm->mOldAvatarFilename != "blank.gif") {
-			@unlink(XOOPS_UPLOAD_PATH . "/" . $this->mActionForm->mOldAvatarFilename);
+		if ($this->mActionForm->mOldFilename != null && $this->mActionForm->mOldFilename != "blank.gif") {
+			@unlink(XOOPS_UPLOAD_PATH . "/" . $this->mActionForm->mOldFilename);
 		}
 
 		return parent::_doExecute();
