3cea886785f03bded31a52e27c8248e01f309f42
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-08 19:53:30 +0000

    if (!defined('XOOPS_ROOT_PATH')) exit();
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@114 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/pt_utf8/html/language/pt_utf8/xoopsmailerlocal.php
+++ extras/extra_languages/pt_utf8/html/language/pt_utf8/xoopsmailerlocal.php
@@ -1,6 +1,7 @@
 <?php
 // $Id$
 // License http://creativecommons.org/licenses/by/2.5/br/
+if (!defined('XOOPS_ROOT_PATH')) exit();
 class XoopsMailerLocal extends XoopsMailer {
 function XoopsMailerLocal(){
 $this->XoopsMailer();
