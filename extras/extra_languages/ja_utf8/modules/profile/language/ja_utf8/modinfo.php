270eaebde85c8a54c4d2737a307b39278976d75a
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-09 17:08:25 +0000

    extra language for module profil, ja_utf8
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@128 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ extras/extra_languages/ja_utf8/modules/profile/language/ja_utf8/modinfo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * @package profile
+ * @version $Id$
+ */
+
+define('_MI_PROFILE_LANG_PROFILE', "プロフィール");
+define('_MI_PROFILE_DESC_PROFILE', "プロフィール");
+define('_MI_PROFILE_LANG_DEFINITIONS_LIST', "プロフィール定義リスト");
+define('_MI_PROFILE_KEYWORD_DEFINITIONS_LIST', "プロフィール定義リスト");
+define('_MI_PROFILE_LANG_ADD_A_NEW_DEFINITIONS', "新しいプロフィール定義を追加する");
+define('_MI_PROFILE_KEYWORD_DEFINITIONS_CREATE', "定義をつくる");
+
+?>
