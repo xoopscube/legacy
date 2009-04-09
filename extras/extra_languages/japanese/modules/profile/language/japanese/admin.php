3e410c49abdb15259be1a8ba23168c5e6d250fb8
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-09 17:00:11 +0000

    extra language for module profile, japanese
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@127 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ extras/extra_languages/japanese/modules/profile/language/japanese/admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * @package profile
+ * @version $Id$
+ */
+
+define('_AD_PROFILE_LANG_DEFINITIONS_EDIT', "プロフィール定義の編集");
+define('_AD_PROFILE_LANG_DEFINITIONS_DELETE', "プロフィール定義の削除");
+define('_AD_PROFILE_LANG_DEFINITIONS_VIEW', "プロフィール定義の表示");
+
+?>
---
+++ extras/extra_languages/japanese/modules/profile/language/japanese/main.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * @package profile
+ * @version $Id$
+ */
+
+define('_MD_PROFILE_ERROR_REQUIRED', "{0}は必ず入力して下さい");
+define('_MD_PROFILE_ERROR_MINLENGTH', "{0}は半角{1}文字以上にして下さい");
+define('_MD_PROFILE_ERROR_MAXLENGTH', "{0}は半角{1}文字以内で入力して下さい");
+define('_MD_PROFILE_ERROR_EXTENSION', "アップロードされたファイルは許可された拡張子と一致しません");
+define('_MD_PROFILE_ERROR_INTRANGE', "{0}の入力値が不正です");
+define('_MD_PROFILE_ERROR_MIN', "{0}は{1}以上の数値を指定して下さい");
+define('_MD_PROFILE_ERROR_MAX', "{0}は{1}以下の数値を指定して下さい");
+define('_MD_PROFILE_ERROR_OBJECTEXIST', "{0}の入力値が不正です");
+define('_MD_PROFILE_ERROR_DBUPDATE_FAILED', "データベースの更新に失敗しました");
+define('_MD_PROFILE_ERROR_EMAIL', "{0}は不正なメールアドレスです");
+define('_MD_PROFILE_MESSAGE_CONFIRM_DELETE', "以下のデータを本当に削除しますか？");
+define('_MD_PROFILE_LANG_ADD_A_NEW_DEFINITIONS', "プロフィール定義を追加する");
+define('_MD_PROFILE_LANG_FIELD_ID', "フィールドID");
+define('_MD_PROFILE_LANG_FIELD_NAME', "フィールド名");
+define('_MD_PROFILE_LANG_LABEL', "表示名");
+define('_MD_PROFILE_LANG_TYPE', "表示タイプ");
+define('_MD_PROFILE_LANG_VALIDATION', "VALIDATION");
+define('_MD_PROFILE_LANG_REQUIRED', "必須設定");
+define('_MD_PROFILE_LANG_SHOW_FORM', "フォームで入力する");
+define('_MD_PROFILE_LANG_WEIGHT', "表示順");
+define('_MD_PROFILE_LANG_DESCRIPTION', "項目説明");
+define('_MD_PROFILE_LANG_ACCESS', "開示グループ指定");
+define('_MD_PROFILE_LANG_OPTIONS', "選択肢");
+define('_MD_PROFILE_LANG_CONTROL', "操作");
+define('_MD_PROFILE_LANG_DEFINITIONS_EDIT', "プロフィール定義の編集");
+define('_MD_PROFILE_LANG_CANCEL', "キャンセル");
+define('_MD_PROFILE_ERROR_CONTENT_IS_NOT_FOUND', "CONTENT_IS_NOT_FOUND");
+define('_MD_PROFILE_LANG_ADD_A_NEW_DATA', "プロフィールを編集する");
+define('_MD_PROFILE_LANG_UID', "ユーザID");
+define('_MD_PROFILE_LANG_DATA_EDIT', "プロフィールを編集する");
+define('_MD_PROFILE_DESC_OPTIONS', "オプションは、| で区切ってください。例：tea|coffee|green tea");
+
+?>
