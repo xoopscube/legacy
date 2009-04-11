da4300dbbd0cbd85b885cf414bba9c462d196d93
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-11 11:31:11 +0000

    Module User lang. catalog 'admin_extra.php'
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@135 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ html/modules/user/language/english/admin_extra.php
@@ -0,0 +1,21 @@
+<?php
+/// here is admin_extra Menu Language Message Catalogue
+
+define('_MI_USER_EXTRA_ADMENU_USER_DATA_DOWNLOAD', 'User Data Download');
+define('_AD_USER_EXTRA_DATA_DOWNLOAD_TIPS1', 'You are only able to get CSV User data order by user_id.');
+define('_AD_USER_EXTRA_DATA_NUM', '%d users are registered.'); 
+define('_AD_USER_EXTRA_DATA_DOWNLOAD_DO', 'Download by CSV');
+
+define('_MI_USER_EXTRA_ADMENU_USER_DATA_CSVUPLOAD', 'The users batch registration');
+define('_AD_USER_EXTRA_DATA_UPLOAD_TIPS1', 'The user batch registration with CSV file is possible.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_TIPS2', 'Use CSV file downloaded from <a href="?action=UserDataDownload" style="color:#941d55;font-weight:bold;">'._MI_USER_EXTRA_ADMENU_USER_DATA_DOWNLOAD.'</a> Do not increase and decrease columns.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_TIPS3', 'Please describe only the user who wants to update and wants to register information newly in CSV file.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_TIPS4', 'When the row of leftmost UID is emptied(or 0), it registers as a new user.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_TIPS5', 'The user information is updated when there is a value of the row of leftmost(UID).<br>If you set password,set it within 30bytes.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_SELECT_USER_CSVFILE', 'Please select the file of registered CSV.');
+define('_AD_USER_EXTRA_DATA_UPLOAD_CHECK_USER_CSVFILE', 'Please confirm the content of registration. ');
+define('_AD_USER_EXTRA_DATA_UPLOAD_CONF', 'Confirm the content of registration');
+define('_AD_USER_EXTRA_DATA_UPLOAD_BACK', 'Select The CSV file again');
+define('_AD_USER_EXTRA_DATA_UPLOAD_DO', 'Register');
+define('_AD_USER_EXTRA_DATA_UPLOAD_DONE', 'The user data was updated according to CSV data.');
+
