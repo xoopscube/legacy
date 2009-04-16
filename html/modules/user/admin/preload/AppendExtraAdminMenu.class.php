1f026bf57ee8aab29d314b494c9dc241b5557448
minahito <minahito@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-16 13:42:55 +0000

    Make loading a method of XOOPS Cube Legacy.
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@149 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/user/admin/preload/AppendExtraAdminMenu.class.php
+++ html/modules/user/admin/preload/AppendExtraAdminMenu.class.php
@@ -17,8 +17,9 @@ class User_AppendExtraAdminMenu extends XCube_ActionFilter
 	/// append extra admin menu
 	function appendExtraAdminMenu(&$menu)
 	{
-		require_once sprintf('%s/modules/user/language/%s/admin_extra.php', 
-							 XOOPS_ROOT_PATH, $GLOBALS['xoopsConfig']['language']);
+		$root =& XCube_Root::getSingleton();
+		$root->mLanguageManager->_loadLanguage("user", "admin_extra.php");
+		
 		$menu[] = array(
 			'title'    => _MI_USER_EXTRA_ADMENU_USER_DATA_DOWNLOAD,
 			'link'     => XOOPS_URL.'/modules/user/admin/index.php?action=UserDataDownload',
