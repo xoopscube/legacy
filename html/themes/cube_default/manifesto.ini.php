489732a4bbada75ffd8db03ba15507381127adce
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-04-19 11:01:21 +0000

    Rename Default Themes
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@152 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ html/themes/cube_default/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="Default theme of Cube"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoopscube.sourceforge.net/"
+Version="1.0.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=XOOPS Cube Project Team
+ScreenShot="screenshot.png"
+Description="Default theme of XOOPS Cube 2.1"
+W3C=NG
+
+License="GPL"
+
+*/
+?>
