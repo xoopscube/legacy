be3138e1ab9994de35b89d96f1b7c1839c8e053f
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-06-23 06:58:16 +0000

    filemanager english modinfo.php
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@301 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/fileManager/language/english/modinfo.php
+++ html/modules/fileManager/language/english/modinfo.php
@@ -22,32 +22,32 @@ define('_MI_FILEMANAGER_FOLDER_DSC', "dirctory control");
 // PreferenceEdit
 // --------------------------------------------------------
 define('_MI_FILEMANAGER_PATH',"Main Upload Directory");
-define('_MI_FILEMANAGER_PATH_DSC',"Please set upload path( under XOOPS_UPLOAD_PATH , / is not need)");
+define('_MI_FILEMANAGER_PATH_DSC',"Define an upload path (under /uploads/, without '/')");
 define('_MI_FILEMANAGER_DIRHANDLE',"Upload directory management");
 define('_MI_FILEMANAGER_DIRHANDLE_DSC',"Check 'Yes' to allow webmasters to create and delete folders.");
 define('_MI_FILEMANAGER_THUMBSIZE',"Thumbnail size");
-define('_MI_FILEMANAGER_THUMBSIZE_DSC',"Specify the thumbnail size for the file list.");
+define('_MI_FILEMANAGER_THUMBSIZE_DSC',"Specify the maximum width of thumbnails for the file list.");
 define('_MI_FILEMANAGER_DEBUGON',"Turn on the debugging Uploader");
-define('_MI_FILEMANAGER_DEBUGON_DSC',"enable upload debuging.");
+define('_MI_FILEMANAGER_DEBUGON_DSC',"Enable or not debug console of upload that is rendered inside an iframe.");
 
 
-define('_MI_FILEMANAGER_XOOPSLOCK',"Non-indication of the system image");
-define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"System using image is nonindication. Image maneger files, Abters, Smiles");
-define('_MI_FILEMANAGER_EXTENSIONS',"Extension of upload files");
-define('_MI_FILEMANAGER_EXTENSIONS_DSC',"The extension is seperate each requirement with |. The extension is need type in all lowercase.");
+define('_MI_FILEMANAGER_XOOPSLOCK',"Hide system images?");
+define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"Display or not files from 'Image Manager'.");
+define('_MI_FILEMANAGER_EXTENSIONS',"File extension allowed for upload");
+define('_MI_FILEMANAGER_EXTENSIONS_DSC',"Separate files extensions with a '|' pipe.<br />Make sure you use all lowercase characters for the file extension.");
 
 // reserved  options setting 
 
-define('_MI_FILEMANAGER_FUSE',"[ffmpeg]using ffmpeg");
-define('_MI_FILEMANAGER_FUSE_DSC',"If you want to use ffmpeg, please select Yes. Ffmpeg is must be supported by the server.<br />Please installed the binary corresponded ffmpeg to your server, or build ffmpeg.");
-define('_MI_FILEMANAGER_FPATH',"[ffmpeg]path for ffmpeg");
-define('_MI_FILEMANAGER_FPATH_DSC',"Specify the installation ffmpeg path.<br />(Example:<tt>/usr/local/bin</tt><tt>:/usr/bin</tt>)");
+define('_MI_FILEMANAGER_FUSE',"[ffmpeg] Use FFmpeg");
+define('_MI_FILEMANAGER_FUSE_DSC',"FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video.<br /> Ffmpeg must be supported by the server. If not, Install the FFmpeg binary to your server.");
+define('_MI_FILEMANAGER_FPATH',"[ffmpeg] Path of FFmpeg");
+define('_MI_FILEMANAGER_FPATH_DSC',"Specify the path installation of FFmpeg.<br />(Example:<tt>/usr/local/bin</tt><tt>:/usr/bin</tt>)");
 define('_MI_FILEMANAGER_FOPT',"[ffmpeg]Option");
 define('_MI_FILEMANAGER_FOPT_DSC',"Please specify the command option.（This version is not available）");
-define('_MI_FILEMANAGER_FCAPTURE',"[ffmpeg]Screen Shot time");
-define('_MI_FILEMANAGER_FCAPTURE_DSC',"Time from the beginning of the video to take a screenshot from the video file (s).");
-define('_MI_FILEMANAGER_FCONVERT',"[ffmpeg]Maximum size of FLV conversion");
-define('_MI_FILEMANAGER_FCONVERT_DSC',"FLV video files to specify the maximum size to be converted to video format. The unit is MB.");
+define('_MI_FILEMANAGER_FCAPTURE',"[ffmpeg] Screen Shot time");
+define('_MI_FILEMANAGER_FCAPTURE_DSC',"Time from the beginning of the video to take a screenshot of the video file(s).");
+define('_MI_FILEMANAGER_FCONVERT',"[ffmpeg] Maximum size of FLV conversion");
+define('_MI_FILEMANAGER_FCONVERT_DSC',"Specify the maximum size of FLV video files to be converted to video format. The unit is MB.");
 
 
 ?>
