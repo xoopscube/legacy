3abd44ff384e3ce263bf33893477e37f868f3291
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-06-29 14:00:21 +0000

    Filemanager english typo
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@322 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/fileManager/language/english/modinfo.php
+++ html/modules/fileManager/language/english/modinfo.php
@@ -5,7 +5,7 @@
   http://beabo.net/
 =====================================================================*/
 define('_MI_FILEMANAGER_NAME', "FileManager");
-define('_MI_FILEMANAGER_DESC', "File and folder management with multiple files upload.");
+define('_MI_FILEMANAGER_DESC', "File and folder management with upload of multiple files.");
 define('_MI_FILEMANAGER_UPDATE', 'Update');
 
 // --------------------------------------------------------
@@ -17,7 +17,7 @@ define('_MI_FILEMANAGER_UPLOAD', "File Upload");
 define('_MI_FILEMANAGER_UPLOAD_DSC', "Upload files to the uploads directory");
 
 define('_MI_FILEMANAGER_FOLDER', "Folder");
-define('_MI_FILEMANAGER_FOLDER_DSC', "dirctory management and permissions");
+define('_MI_FILEMANAGER_FOLDER_DSC', "Directory management and permissions");
 // --------------------------------------------------------
 // PreferenceEdit
 // --------------------------------------------------------
@@ -28,12 +28,12 @@ define('_MI_FILEMANAGER_DIRHANDLE_DSC',"Allow or not webmasters to create and de
 define('_MI_FILEMANAGER_THUMBSIZE',"Thumbnail size");
 define('_MI_FILEMANAGER_THUMBSIZE_DSC',"Specify the maximum width of thumbnails for the file list.");
 define('_MI_FILEMANAGER_DEBUGON',"Turn on the debugging Uploader");
-define('_MI_FILEMANAGER_DEBUGON_DSC',"Enable or not debug console of uploader that is rendered inside an iframe.");
+define('_MI_FILEMANAGER_DEBUGON_DSC',"Enable or not the debug console of uploader that is rendered inside an iframe.");
 
 
 define('_MI_FILEMANAGER_XOOPSLOCK',"Hide system images?");
 define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"Display or not files from 'Image Manager' (ie. avatars, smilies, etc");
-define('_MI_FILEMANAGER_EXTENSIONS',"File extension allowed for upload");
+define('_MI_FILEMANAGER_EXTENSIONS',"File extensions allowed for upload");
 define('_MI_FILEMANAGER_EXTENSIONS_DSC',"Separate files extensions with a '|' pipe.<br />Make sure you use all lowercase characters for the file extension.");
 
 // reserved  options setting 
@@ -43,11 +43,11 @@ define('_MI_FILEMANAGER_FUSE_DSC',"FFmpeg is a complete, cross-platform solution
 define('_MI_FILEMANAGER_FPATH',"[ffmpeg] Path of FFmpeg");
 define('_MI_FILEMANAGER_FPATH_DSC',"Specify the path installation of FFmpeg.<br />(Example:<tt>/usr/local/bin</tt>, <tt>/usr/bin</tt>)");
 define('_MI_FILEMANAGER_FOPT',"[ffmpeg] Option");
-define('_MI_FILEMANAGER_FOPT_DSC',"Please specify the command option.（This version is not available）");
+define('_MI_FILEMANAGER_FOPT_DSC',"Please specify the command option.（This feature is not available）");
 define('_MI_FILEMANAGER_FCAPTURE',"[ffmpeg] Screen Shot time");
 define('_MI_FILEMANAGER_FCAPTURE_DSC',"Time from the beginning of the video to take a screenshot.");
 define('_MI_FILEMANAGER_FCONVERT',"[ffmpeg] Maximum size of FLV conversion");
-define('_MI_FILEMANAGER_FCONVERT_DSC',"Specify the maximum size of FLV video files to be converted to video format. The unit is MB.");
+define('_MI_FILEMANAGER_FCONVERT_DSC',"Specify the maximum size of files converted to video format FLV. The unit is MB.");
 
 
 ?>
