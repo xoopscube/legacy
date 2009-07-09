6a3101135528a37766038aab8a154bd898e02c6a
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-07-09 23:42:22 +0000

    Update Filemanager ja_utf8
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@325 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/ja_utf8/modules/filemanager/language/ja_utf8/admin.php
+++ extras/extra_languages/ja_utf8/modules/filemanager/language/ja_utf8/admin.php
@@ -1,16 +1,15 @@
 <?php
-/*=====================================================================
-  (C)2007 BeaBo Japan by Hiroki Seike
-  http://beabo.net/
-=====================================================================*/
+/**
+ * Filemaneger
+ * (C)2007-2009 BeaBo Japan by Hiroki Seike
+ * http://beabo.net/
+ **/
 
 // --------------------------------------------------------
 // メイン
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_MAIN_DSC', "現在登録されているメディアの一覧です。");
 define('_AD_FILEMANAGER_PATH_HOME', "ホーム");
 define('_AD_FILEMANAGER_TYPE', "タイプ");
-define('_AD_FILEMANAGER_PARENT', "上へ");
 define('_AD_FILEMANAGER_EDIT', "追加");
 define('_AD_FILEMANAGER_DEL', "削除");
 define('_AD_FILEMANAGER_RETURN', "リストへ戻る");
@@ -27,21 +26,24 @@ define('_AD_FILEMANAGER_ERROR_PERMISSION', "アクセス権限がありません
 define('_AD_FILEMANAGER_ERROR_FILE_PERMISSION', "%s は、アクセス権限がありません。");
 define('_AD_FILEMANAGER_ERROR_DELETE_FOR_PERMISSION', "%s は、削除するアクセス権限がない為ファイルマネージャーから削除できません。");
 define('_AD_FILEMANAGER_NOTFOUND', "ファイルが見つかりません。");
+define('_AD_FILEMANAGER_ERROR_FILE_EXISTS', "'%s' が見つかりません。");
 
 // --------------------------------------------------------
 // アップロード
 // --------------------------------------------------------
 define('_AD_FILEMANAGER_PREVIEW', "プレビュー");
 define('_AD_FILEMANAGER_FILENAME', "ファイル");
+define('_AD_FILEMANAGER_FOLDER_ADD', "ディレクトリ追加");
 define('_AD_FILEMANAGER_SIZE', "サイズ");
 define('_AD_FILEMANAGER_DATE', "更新日時");
 define('_AD_FILEMANAGER_UPLOAD', "アップロード");
 define('_AD_FILEMANAGER_UPLOAD_DSC', "Uploadをクリックして、ファイルを選択するとアップロードを開始します。");
 define('_AD_FILEMANAGER_UPLOAD_NOTACCESS',  "%s は、アップロードできません。FTPソフトなどで、パーミッションを変更して下さい。");
+define('_AD_FILEMANAGER_UPLOAD_PERMISSION', "アップロードパスが見つからないか、アップロード権限がありません。");
+define('_AD_FILEMANAGER_UPLOAD_NOFILE', "アップロードするファイルが見つかりません。ファイルを選択して下さい。");
+define('_AD_FILEMANAGER_SINGLEUPLOAD', "ファイルアップロード");
 define('_AD_FILEMANAGER_NOTFOUNDURL', "アップロードパスが見つかりません。");
 define('_AD_FILEMANAGER_CONFIRMMSSAGE', "%s へファイルをアップロードします。<br />アップロード可能なファイルサイズは %sまで可能です。");
-define('_AD_FILEMANAGER_UPLOAD_PERMISSION', "アップロードパスが見つからないか、アップロード権限がありません。");
-define('_AD_FILEMANAGER_FOLDER_ADD', "ディレクトリ追加");
 
 // --------------------------------------------------------
 // フォルダ
@@ -51,13 +53,11 @@ define('_AD_FILEMANAGER_FOLDERNAME', "フォルダ名");
 define('_AD_FILEMANAGER_FOLDER_UPLOAD', "このフォルダにアップロード");
 define('_AD_FILEMANAGER_ERROR_FOLDERNAME', "フォルダ名が正しくありません。フォルダ名を確認して下さい。<br />フォルダ名で使える文字は、半角英数字-~_のみが利用出来ます。<br />英字は小文字のみが利用出来ます。");
 define('_AD_FILEMANAGER_ERROR_PATH', "フォルダ名の指定が正しくありません。フォルダ名を確認して下さい。");
-define('_AD_FILEMANAGER_ADD', "ﾌｫﾙﾀﾞ追加");
 define('_AD_FILEMANAGER_ADDFOLDER', "フォルダの追加");
 define('_AD_FILEMANAGER_ADDFOLDER_DSC', "新しいフォルダを追加します。作成されたフォルダはファイルマネージャーから操作できます。");
 define('_AD_FILEMANAGER_ADDFOLDER_SUCCESS', "フォルダを追加しました。");
 define('_AD_FILEMANAGER_ADDFOLDER_ERROR', "フォルダがないか、アクセス権限がない為、フォルダを追加出来ません。");
 define('_AD_FILEMANAGER_ADDFOLDER_CONFIRMMSSAGE', "%s の下にフォルダを作成します。<br />作成するフォルダ名を入力して下さい。");
-define('_AD_FILEMANAGER_DELET', "ﾌｫﾙﾀﾞ削除");
 define('_AD_FILEMANAGER_DELFOLDER', "フォルダの削除");
 define('_AD_FILEMANAGER_DELFOLDER_DSC', "指定したフォルダを削除します。");
 define('_AD_FILEMANAGER_DELFOLDER_CONFIRMMSSAGE', "フォルダ %s を削除します。");
@@ -79,23 +79,23 @@ define('_AD_FILEMANAGER_SWF_LOAD_HAS_FAILED', "SWFUpload ライブラリをロ�
 define('_AD_FILEMANAGER_SWF_INSTALL_FLASH', "SWFUpload  ライブラリをロード出来ません。ライブラリがセットされているか確認するか、Flash プレイヤーをインストールして下さい。<br />こちらから <a href=\"http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\">Adobe website</a>Flash プレイヤーをインストールして下さい");
 
 // --------------------------------------------------------
-// アップロード
+// 設定確認
 // --------------------------------------------------------
 define('_AD_FILEMANAGER_OPTION', "オプション");
 define('_AD_FILEMANAGER_OPTION_DSC', "オプション");
+define('_AD_FILEMANAGER_CHECK_NG', "ライブラリのファイルが見つかりません。ファイルをアップロードして下さい。<br />");
+define('_AD_FILEMANAGER_CHECK_OK', "ライブラリファイルの設置は、完了しています。");
+define('_AD_FILEMANAGER_CHECK', "動作環境の確認");
+define('_AD_FILEMANAGER_CHECK_DSC_1', "SWFUploadを利用する場合");
+define('_AD_FILEMANAGER_CHECK_DSC_2', "SWFUploadを使ったアップロードを一般に公開するのはオススメできません。<br />ライブラリのある場所へhtaccessを使ったアクセス制限を設定してください。");
+define('_AD_FILEMANAGER_HTACCESS_DSC_1', "下記フォームをクリックすると設定内容を選択できます。");
+define('_AD_FILEMANAGER_HTACCESS_DSC_2', "現在アクセスしているIPアドレスのみ許可する場合（自動的に組成したサンプルです。お使いのサーバ環境に合わせて変更して下さい。）");
+define('_AD_FILEMANAGER_HTACCESS_PATH', "htaccessファイルを設置するパス");
 
 // --------------------------------------------------------
-// reserved  options setting
+// FFMPEG 操作
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_FILTER', "フィルター");
-define('_AD_FILEMANAGER_FILTER_ALL', "---");
-define('_AD_FILEMANAGER_FILTER_IMGAE', "画像");
-define('_AD_FILEMANAGER_FILTER_MOVIE', "動画");
-define('_AD_FILEMANAGER_FILTER_SOUND', "音楽");
-define('_AD_FILEMANAGER_FILTER_APLICATION', "アプリ");
 define('_AD_FILEMANAGER_ACTION_CONVERT', "FLV変換");
 define('_AD_FILEMANAGER_ACTION_CAPTURE', "キャプチャー画像作成");
-define('_AD_FILEMANAGER_CONVERT_DSC', "指定された動画ファイルをFLV形式に変換中です。");
-define('_AD_FILEMANAGER_CONVERT_NOW', "FLVファイルに変換作業中です。変換が完了するまでしばらくお待ちください。<br />変換が終わったら元の画面へ戻ります。 ");
 
 ?>
---
+++ extras/extra_languages/ja_utf8/modules/filemanager/language/ja_utf8/todo-admin.php
@@ -0,0 +1,16 @@
+<?php
+// --------------------------------------------------------
+// reserved  options setting
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_FILTER', "フィルター");
+define('_AD_FILEMANAGER_FILTER_ALL', "---");
+define('_AD_FILEMANAGER_FILTER_IMGAE', "画像");
+define('_AD_FILEMANAGER_FILTER_MOVIE', "動画");
+define('_AD_FILEMANAGER_FILTER_SOUND', "音楽");
+define('_AD_FILEMANAGER_FILTER_APLICATION', "アプリ");
+define('_AD_FILEMANAGER_ACTION_CONVERT', "FLV変換");
+define('_AD_FILEMANAGER_ACTION_CAPTURE', "キャプチャー画像作成");
+define('_AD_FILEMANAGER_CONVERT_DSC', "指定された動画ファイルをFLV形式に変換中です。");
+define('_AD_FILEMANAGER_CONVERT_NOW', "FLVファイルに変換作業中です。変換が完了するまでしばらくお待ちください。<br />変換が終わったら元の画面へ戻ります。 ");
+
+?>
