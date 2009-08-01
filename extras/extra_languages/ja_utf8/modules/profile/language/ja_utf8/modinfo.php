b4ddf823dfe55402651f2513f7180a0021e7d43f
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-08-01 12:28:34 +0000

    update language files for profile download func
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@339 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/ja_utf8/modules/profile/language/ja_utf8/modinfo.php
+++ extras/extra_languages/ja_utf8/modules/profile/language/ja_utf8/modinfo.php
@@ -11,5 +11,9 @@ define('_MI_PROFILE_LANG_DEFINITIONS_LIST', "プロフィール定義リスト")
 define('_MI_PROFILE_KEYWORD_DEFINITIONS_LIST', "プロフィール定義リスト");
 define('_MI_PROFILE_LANG_ADD_A_NEW_DEFINITIONS', "新しいプロフィール定義を追加する");
 define('_MI_PROFILE_KEYWORD_DEFINITIONS_CREATE', "定義をつくる");
+define("_MI_PROFILE_NAME", "プロフィールモジュール");
+define("_MI_PROFILE_ADMENU_DATA_DOWNLOAD", "ダウンロード");
+define("_MI_PROFILE_LANG_DOWNLOAD", "ダウンロード");
+define("_MI_PROFILE_KEYWORD_DOWNLOAD", "csv形式でプロフィールデータをダウンロードします。");
 
 ?>
--- extras/extra_languages/japanese/modules/profile/language/japanese/admin.php
+++ extras/extra_languages/japanese/modules/profile/language/japanese/admin.php
@@ -8,5 +8,8 @@
 define('_AD_PROFILE_LANG_DEFINITIONS_EDIT', "プロフィール定義の編集");
 define('_AD_PROFILE_LANG_DEFINITIONS_DELETE', "プロフィール定義の削除");
 define('_AD_PROFILE_LANG_DEFINITIONS_VIEW', "プロフィール定義の表示");
+define("_AD_PROFILE_TIPS_DATA_DOWNLOAD", "ユーザID順に並んだデータをCSV形式でダウンロードすることができます。");
+define("_AD_PROFILE_DATA_NUM", "%d 人のプロフィールが登録されています。");
+define("_AD_PROFILE_DATA_DOWNLOAD_DO", "CSVファイルをダウンロード");
 
 ?>
