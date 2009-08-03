34f909448c0092804bd734cac6b765a6bb302d9a
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-08-03 22:55:27 +0000

    fileManager module: Portuguese language files.
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@343 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/portuguese/modules/fileManager/language/portuguese/modinfo.php
+++ extras/extra_languages/portuguese/modules/fileManager/language/portuguese/modinfo.php
@@ -1,59 +1,70 @@
 <?php
+// Translation Info
+// $Id$
+// License http://creativecommons.org/licenses/by/2.5/br/
+// ############################################################### //
+// ## XOOPS Cube Legacy 2.1 - Tradução para o Português do Brasil
+// ############################################################### //
+// ## Por............: Mikhail Miguel
+// ## E-mail.........: mikhail@underpop.com
+// ## Website........: http://xoopscube.com.br
+// ############################################################### //
+// *************************************************************** //
 /**
  * Filemaneger
  * (C)2007-2009 BeaBo Japan by Hiroki Seike
  * http://beabo.net/
  **/
 
-define('_MI_FILEMANAGER_NAME', "FileManager");
-define('_MI_FILEMANAGER_DESC', "File and folder management with upload of multiple files.");
-define('_MI_FILEMANAGER_UPDATE', 'Update');
+define("_MI_FILEMANAGER_NAME","Arquivos");
+define("_MI_FILEMANAGER_DESC","Gerenciador de pastas e arquivos, com opção para envio em lote.");
+define("_MI_FILEMANAGER_UPDATE","Atualizar");
 
 // --------------------------------------------------------
 // Names of admin menu items
 // --------------------------------------------------------
-define('_MI_FILEMANAGER_MAIN', "File List");
-define('_MI_FILEMANAGER_MAIN_DSC', "File list with thumbnails and image viewer");
-define('_MI_FILEMANAGER_UPLOAD', "File Upload");
-define('_MI_FILEMANAGER_UPLOAD_DSC', "Upload files to the uploads directory");
+define("_MI_FILEMANAGER_MAIN","Lista de arquivos");
+define("_MI_FILEMANAGER_MAIN_DSC","lista de arquivos com miniaturas visualizador de imagens");
+define("_MI_FILEMANAGER_UPLOAD","Enviar arquivo");
+define("_MI_FILEMANAGER_UPLOAD_DSC","Enviar arquivos para o diretório <q>uploads</q>");
 
-define('_MI_FILEMANAGER_FOLDER', "Folder");
-define('_MI_FILEMANAGER_FOLDER_DSC', "directory management and permissions");
+define("_MI_FILEMANAGER_FOLDER","Pasta");
+define("_MI_FILEMANAGER_FOLDER_DSC","Gestão de diretórios e suas permissões");
 
-define('_MI_FILEMANAGER_CHECK', "Setting Check");
-define('_MI_FILEMANAGER_CHECK_DSC', "Check the settings in the file manager.");
+define("_MI_FILEMANAGER_CHECK","Verificar configuração");
+define("_MI_FILEMANAGER_CHECK_DSC","Verifique as configurações do gerenciador de arquivos");
 
 // --------------------------------------------------------
 // PreferenceEdit
 // --------------------------------------------------------
-define('_MI_FILEMANAGER_PATH',"Main Upload Directory");
-define('_MI_FILEMANAGER_PATH_DSC',"Define the upload path under /uploads/ (folder name only, without '/' slash)");
-define('_MI_FILEMANAGER_DIRHANDLE',"Directory Management");
-define('_MI_FILEMANAGER_DIRHANDLE_DSC',"Allow or not webmasters to create and delete folders.");
-define('_MI_FILEMANAGER_THUMBSIZE',"Thumbnail size");
-define('_MI_FILEMANAGER_THUMBSIZE_DSC',"Specify the maximum width of thumbnails for the file list.");
-define('_MI_FILEMANAGER_DEBUGON',"Turn on the debugging Uploader");
-define('_MI_FILEMANAGER_DEBUGON_DSC',"Enable or not debug console of uploader that is rendered inside an iframe.");
+define("_MI_FILEMANAGER_PATH","Principal diretório para envios");
+define("_MI_FILEMANAGER_PATH_DSC","Defina o caminho sob /uploads/ (nome da pasta apenas, sem a barra no fim)");
+define("_MI_FILEMANAGER_DIRHANDLE","Gestor de diretórios");
+define("_MI_FILEMANAGER_DIRHANDLE_DSC","Permitir ou não webmasters para criar e apagar pastas");
+define("_MI_FILEMANAGER_THUMBSIZE","Tamanho da miniatura");
+define("_MI_FILEMANAGER_THUMBSIZE_DSC","Especifique a largura máxima das miniaturas para a lista de arquivos");
+define("_MI_FILEMANAGER_DEBUGON","Habilitar o modo de depuração do gerenciador");
+define("_MI_FILEMANAGER_DEBUGON_DSC","Ativar ou não console debug do uploader que é prestado dentro de um iframe");
 
 
-define('_MI_FILEMANAGER_XOOPSLOCK',"Hide system images?");
-define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"Display or not files from 'Image Manager' (ie. avatars, smilies, etc");
-define('_MI_FILEMANAGER_EXTENSIONS',"File extension allowed for upload");
-define('_MI_FILEMANAGER_EXTENSIONS_DSC',"Separate files extensions with a '|' pipe.<br />Make sure you use all lowercase characters for the file extension.<br />Default setting is gif|jpg|jpeg|png|avi|mov|wmv|mp3|mp4|flv|doc|xls|ods|odt|pdf");
+define("_MI_FILEMANAGER_XOOPSLOCK","Ocultar imagens do sistema?");
+define("_MI_FILEMANAGER_XOOPSLOCK_DSC","Mostra ou não arquivos de 'Image Manager' (ie. avatars, smilies, etc)");
+define("_MI_FILEMANAGER_EXTENSIONS","Extensões dos arquivos permitidos para serem enviados ao servidor");
+define("_MI_FILEMANAGER_EXTENSIONS_DSC","Separe as extensões dos formatos dos arquivos com barras verticais.<br /> Certifique-se de utilizar apenas minúsculas e não incluir espações entre elas as extensões. <br />O valor predefinido é: <q>gif|jpg|jpeg|png|avi|mov|wmv|mp3|mp4|flv|doc|xls|ods|odt|pdf</q>");
 
-// reserved  options setting 
+// reserved  options setting
 
-define('_MI_FILEMANAGER_FUSE',"[ffmpeg] Use FFmpeg");
-define('_MI_FILEMANAGER_FUSE_DSC',"FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video.<br /> FFmpeg must be supported by the server. If not, Install the FFmpeg binary to your server.");
-define('_MI_FILEMANAGER_FPATH',"[ffmpeg] Path of FFmpeg");
-define('_MI_FILEMANAGER_FPATH_DSC',"Specify the path installation of FFmpeg.<br />(Example:<tt>/usr/local/bin</tt>, <tt>/usr/bin</tt>)");
-define('_MI_FILEMANAGER_FOPT',"[ffmpeg] Option");
-define('_MI_FILEMANAGER_FOPT_DSC',"Please specify the command option.ãƒ»ãƒ»his version is not availableãƒ»ãƒ»");
-define('_MI_FILEMANAGER_FCAPTURE',"[ffmpeg] Screen Shot time");
-define('_MI_FILEMANAGER_FCAPTURE_DSC',"Time from the beginning of the video to take a screenshot.");
-define('_MI_FILEMANAGER_FCONVERT',"[ffmpeg] Maximum size of FLV conversion");
-define('_MI_FILEMANAGER_FCONVERT_DSC',"Specify the maximum size of FLV video files to be converted to video format. The unit is MB.");
-define('_MI_FILEMANAGER_FMOVIEFILE',"[ffmpeg] File format to convert to FLV");
-define('_MI_FILEMANAGER_FMOVIEFILE_DSC',"Separate files format extensions with a '|' pipe.<br />Make sure you use all lowercase characters for the file extension.<br />File format is convert in ffmpeg setting.<br />Default setting is flv|avi|mwv|mov|mpg|qt|mov|3gp|3gp2|mp4");
+define("_MI_FILEMANAGER_FUSE","[ffmpeg] Utilizar FFmpeg");
+define("_MI_FILEMANAGER_FUSE_DSC","FFmpeg é uma completa, solução multi-plataforma para gravar, converter e stream de áudio e vídeo. <br /> FFmpeg deve ser suportada pelo servidor. Se não estiver, instale o FFmpeg binários para o seu servidor");
+define("_MI_FILEMANAGER_FPATH","[ffmpeg] Caminho do FFmpeg");
+define("_MI_FILEMANAGER_FPATH_DSC","Especifique o caminho de instalação FFmpeg. <br /> (Exemplo: <tt> / usr / local / bin </ tt>, <tt> / usr / bin </ tt >)");
+define("_MI_FILEMANAGER_FOPT","[ffmpeg] Opção");
+define("_MI_FILEMANAGER_FOPT_DSC","Especifique o comando opção. Sua versão não está disponível");
+define("_MI_FILEMANAGER_FCAPTURE","[ffmpeg] tempo para o Screen Shot ");
+define("_MI_FILEMANAGER_FCAPTURE_DSC","Tempo a partir do início do vídeo para ter uma imagem");
+define("_MI_FILEMANAGER_FCONVERT","[ffmpeg] tamanho máximo de FLV conversão");
+define("_MI_FILEMANAGER_FCONVERT_DSC","Especifique o tamanho máximo de arquivos FLV vídeo a ser convertido para o formato video. A unidade é MB");
+define("_MI_FILEMANAGER_FMOVIEFILE","[ffmpeg] Formato do arquivo para converter para FLV");
+define("_MI_FILEMANAGER_FMOVIEFILE_DSC","Separe as extensões dos formatos dos arquivos com barras verticais.<br />Certifique-se de utilizar todas as palavras em minúsculas e de não incluir espaços entre elas. <br /> O valor predefinido é <q>flv|avi|mwv|mov|mpg|qt|mov|3gp|3gp2|mp4</q>");
 
 ?>
--- extras/extra_languages/pt_utf8/modules/fileManager/language/pt_utf8/admin.php
+++ extras/extra_languages/pt_utf8/modules/fileManager/language/pt_utf8/admin.php
@@ -1,98 +1,103 @@
 <?php
-/**
- * Filemaneger
- * (C)2007-2009 BeaBo Japan by Hiroki Seike
- * http://beabo.net/
- **/
+// Translation Info
+// $Id$
+// License http://creativecommons.org/licenses/by/2.5/br/
+// ############################################################### //
+// ## XOOPS Cube Legacy 2.1 - Tradução para o Português do Brasil
+// ############################################################### //
+// ## Por............: Mikhail Miguel
+// ## E-mail.........: mikhail@underpop.com
+// ## Website........: http://xoopscube.com.br
+// ############################################################### //
+// *************************************************************** //
 
 // --------------------------------------------------------
 // Main
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_MAIN_DSC', "List of file uploads. When you click a folder to move to the folder. The big picture and video preview is available.");
-define('_AD_FILEMANAGER_PATH_HOME', "Home");
-define('_AD_FILEMANAGER_TYPE', "Type");
-define('_AD_FILEMANAGER_EDIT', "Add");
-define('_AD_FILEMANAGER_DEL', "Delete");
-define('_AD_FILEMANAGER_PARENT', "Back");
-define('_AD_FILEMANAGER_RETURN', "Return to List");
-define('_AD_FILEMANAGER_ACTION_DELETE', "&nbsp;Delete&nbsp;");
-define('_AD_FILEMANAGER_ACTION_DEFULT', "&nbsp;Submit&nbsp; ");
-define('_AD_FILEMANAGER_ACTION_SUBMIT', "&nbsp;Submit&nbsp;");
-define('_AD_FILEMANAGER_FILE_TOTAL', "Total");
+define("_AD_FILEMANAGER_MAIN_DSC","Lista dos carregamentos. Quando você clica em uma pasta para mover para a pasta. A grande foto e vídeo preview está disponível");
+define("_AD_FILEMANAGER_PATH_HOME","Inicial");
+define("_AD_FILEMANAGER_TYPE","Tipo");
+define("_AD_FILEMANAGER_EDIT","Incluir");
+define("_AD_FILEMANAGER_DEL","Remover");
+define("_AD_FILEMANAGER_PARENT","Retornar");
+define("_AD_FILEMANAGER_RETURN","Retornar à lista");
+define("_AD_FILEMANAGER_ACTION_DELETE","&nbsp;Remover&nbsp;");
+define("_AD_FILEMANAGER_ACTION_DEFULT","&nbsp;Enviar&nbsp;");
+define("_AD_FILEMANAGER_ACTION_SUBMIT","&nbsp;Enviar&nbsp;");
+define("_AD_FILEMANAGER_FILE_TOTAL","Total");
 
 // --------------------------------------------------------
 // Error
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_ERROR_REQUIRED', "{0} is required.");
-define('_AD_FILEMANAGER_ERROR_PERMISSION', "There is no access permission.");
-define('_AD_FILEMANAGER_ERROR_DELETE_FOR_PERMISSION', "You do not have access %s to the file manager can not be removed from.");
-define('_AD_FILEMANAGER_NOTFOUND', "File not found.");
+define("_AD_FILEMANAGER_ERROR_REQUIRED","{0} é um valor necessário");
+define("_AD_FILEMANAGER_ERROR_PERMISSION","Não há permissão de acesso para essa operação.");
+define("_AD_FILEMANAGER_ERROR_DELETE_FOR_PERMISSION","Você não tem acesso do tipo <q>%s</q> para o gerenciador de arquivos.");
+define("_AD_FILEMANAGER_NOTFOUND","Arquivo não encontrado.");
 
 // --------------------------------------------------------
 // Uploads
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_PREVIEW', "Preview");
-define('_AD_FILEMANAGER_FILENAME', "File");
-define('_AD_FILEMANAGER_SIZE', "Size");
-define('_AD_FILEMANAGER_DATE', "Update Time");
-define('_AD_FILEMANAGER_UPLOAD', "Upload");
-define('_AD_FILEMANAGER_UPLOAD_DSC', "Click upload to select for your computer's files and start the upload.");
-define('_AD_FILEMANAGER_UPLOAD_NOTACCESS',  "%s is not upload. FTP software, please change the permissions.");
-define('_AD_FILEMANAGER_NOTFOUNDURL', "Upload Paht is not fuound");
-define('_AD_FILEMANAGER_CONFIRMMSSAGE', "You can upload up to max %s .");
-define('_AD_FILEMANAGER_FOLDER_ADD', "Add Folder");
+define("_AD_FILEMANAGER_PREVIEW","Mostrar");
+define("_AD_FILEMANAGER_FILENAME","Arquivo");
+define("_AD_FILEMANAGER_SIZE","Tamanho");
+define("_AD_FILEMANAGER_DATE","Tempo da atualização");
+define("_AD_FILEMANAGER_UPLOAD","Enviar");
+define("_AD_FILEMANAGER_UPLOAD_DSC","Clique para seleccionar para carregar os arquivos do seu computador e iniciar o upload");
+define("_AD_FILEMANAGER_UPLOAD_NOTACCESS","Não foi possível enviar %s. Por favor, altere as permissões manualmente (por exemplo, com um software FTP).");
+define("_AD_FILEMANAGER_NOTFOUNDURL","O local onde o arquivo seria gravado não foi encontrado, possivelmente não existe.");
+define("_AD_FILEMANAGER_CONFIRMMSSAGE","Você pode enviar arquivos de no máximo %s.");
+define("_AD_FILEMANAGER_FOLDER_ADD","Criar subdiretório");
 
 // --------------------------------------------------------
 // Folder
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_FOLDER', "Folder");
-define('_AD_FILEMANAGER_FOLDERNAME', "New Folder name");
-define('_AD_FILEMANAGER_ERROR_FOLDERNAME', "Folder name is not correct.<br />Characters can be used in the name of the folder, and alphanumeric characters -_.");
-define('_AD_FILEMANAGER_ERROR_PATH', "older name is not right. Please confirm a folder name.");
-define('_AD_FILEMANAGER_ADD', "Add Folder");
-define('_AD_FILEMANAGER_ADDFOLDER', "Add Folder");
-define('_AD_FILEMANAGER_ADDFOLDER_DSC', "Create new Folder under XOOPS uploads dirctory.");
-define('_AD_FILEMANAGER_ADDFOLDER_SUCCESS', "Add Folder.");
-define('_AD_FILEMANAGER_ADDFOLDER_ERROR', "I can not add a folder.");
-define('_AD_FILEMANAGER_ADDFOLDER_CONFIRMMSSAGE', "Create a folder under the %s <br />Add a name for the new folder.");
-define('_AD_FILEMANAGER_DELET', "Delete");
-define('_AD_FILEMANAGER_DELFOLDER', "Delete Folder");
-define('_AD_FILEMANAGER_DELFOLDER_DSC', "Remove the specified folder.");
-define('_AD_FILEMANAGER_DELFOLDER_CONFIRMMSSAGE', "Delete folder %s");
-define('_AD_FILEMANAGER_DELFOLDER_FILE_EXISTS', "%s for the file to the folder that can not be deleted. To verify the contents of a folder, please.");
-define('_AD_FILEMANAGER_DELFOLDER_SUCCESS', "Deleted folder.");
-define('_AD_FILEMANAGER_DELFOLDER_ERROR', "I can not delete the folder. Not specify an empty folder, or is not appropriate permissions.");
-define('_AD_FILEMANAGER_DELFOLDER_ISDIR', "Folder %s is can not be removed from the file manager.");
-define('_AD_FILEMANAGER_DELFOLDER_NOTACCESS', "Folder %s is can not be removed from the file manager. FTP software, please change the permissions.");
-define('_AD_FILEMANAGER_FILECOUNT', "Files");
+define("_AD_FILEMANAGER_FOLDER","Diretório");
+define("_AD_FILEMANAGER_FOLDERNAME","Novo nome para o diretório");
+define("_AD_FILEMANAGER_ERROR_FOLDERNAME","O nome da pasta não está correto. <br /> Apenas travessão, hifem e caracteres alfanuméricos e podem ser utilizados.");
+define("_AD_FILEMANAGER_ERROR_PATH","mais velhos nome não é certa. Favor confirmar um nome de pasta");
+define("_AD_FILEMANAGER_ADD","Criar diretório");
+define("_AD_FILEMANAGER_ADDFOLDER","Criar diretório");
+define("_AD_FILEMANAGER_ADDFOLDER_DSC","Criar um subdiretório sob o diretório <q>uploads</q> do XOOPS Cube.");
+define("_AD_FILEMANAGER_ADDFOLDER_SUCCESS","Adicionar pasta");
+define("_AD_FILEMANAGER_ADDFOLDER_ERROR","Eu não posso criar um diretório");
+define("_AD_FILEMANAGER_ADDFOLDER_CONFIRMMSSAGE","Criar uma pasta sob %s. <br />Escreva o nome para a nova pasta.");
+define("_AD_FILEMANAGER_DELET","Remover");
+define("_AD_FILEMANAGER_DELFOLDER","Remover diretório");
+define("_AD_FILEMANAGER_DELFOLDER_DSC","Remova o diretório selecionado.");
+define("_AD_FILEMANAGER_DELFOLDER_CONFIRMMSSAGE","Remover o diretório %s");
+define("_AD_FILEMANAGER_DELFOLDER_FILE_EXISTS","%s para o arquivo para a pasta que não pode ser apagada. Para verificar o conteúdo de uma pasta, por favor");
+define("_AD_FILEMANAGER_DELFOLDER_SUCCESS","Pasta removida corretamente.");
+define("_AD_FILEMANAGER_DELFOLDER_ERROR","Eu não posso apagar a pasta. Não especificar uma pasta vazia, ou não é adequado permissões");
+define("_AD_FILEMANAGER_DELFOLDER_ISDIR","O diretório %s não pode ser removido do gerenciador de arquivos");
+define("_AD_FILEMANAGER_DELFOLDER_NOTACCESS","O diretório %s não pode ser removido pelo gerenciador de arquivos. Por favor, altere as permissões manualmente via software FTP.");
+define("_AD_FILEMANAGER_FILECOUNT","Arquivos");
 
 // --------------------------------------------------------
 // Setting check
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_CHECK_NG', "Library file is not found.Please upload files.<br />");
-define('_AD_FILEMANAGER_CHECK_OK', "The setting of the library file is completed.");
-define('_AD_FILEMANAGER_CHECK', "Setting Check");
-define('_AD_FILEMANAGER_CHECK_DSC_1', "Using SWFUpload");
-define('_AD_FILEMANAGER_CHECK_DSC_2', "SWFUpload to upload for public use is not recommended.<br />The htaccess set to the location of the library, please set up with restricted access.");
-define('_AD_FILEMANAGER_HTACCESS_DSC_1', "You can choose your settings and click the form");
-define('_AD_FILEMANAGER_HTACCESS_DSC_2', "That allow access your current IP address.(Sample composition is automatic. Please change to suit your server environment.)");
-define('_AD_FILEMANAGER_HTACCESS_PATH', "htaccess file to install path");
+define("_AD_FILEMANAGER_CHECK_NG","O arquivo da biblioteca não foi encontrado. Por favor, envie esses arquivos ao servidor. <br />");
+define("_AD_FILEMANAGER_CHECK_OK","A configuração do arquivo estiver concluída");
+define("_AD_FILEMANAGER_CHECK","Verificar configuração");
+define("_AD_FILEMANAGER_CHECK_DSC_1","SWFUpload em uso");
+define("_AD_FILEMANAGER_CHECK_DSC_2","SWFUpload para upload para uso público não é recomendada. <br /> O htaccess para definir a localização da biblioteca, por favor, criada com acesso restrito");
+define("_AD_FILEMANAGER_HTACCESS_DSC_1","Você pode escolher as suas configurações e clique no formulário");
+define("_AD_FILEMANAGER_HTACCESS_DSC_2","Que permitem aceder ao seu endereço IP atual. A composição da amostra é gerado automático. Favor mude para se adequar ao ambiente do seu servidor.");
+define("_AD_FILEMANAGER_HTACCESS_PATH","arquivo .htaccess para o caminho de instalação");
 
 // --------------------------------------------------------
 // SWFUpload
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_SWF_UPLOAD_QUEUE', "Uploads");
-define('_AD_FILEMANAGER_SWF_UPLOAD_CNACEL', "Cancel All Uploads");
-define('_AD_FILEMANAGER_SWF_COULD_NOT_LOAD', "We're sorry.  SWFUpload could not load.  You must have JavaScript enabled to enjoy SWFUpload.");
-define('_AD_FILEMANAGER_SWF_LOADING', "SWFUpload is loading. Please wait a moment...");
-define('_AD_FILEMANAGER_SWF_LOAD_HAS_FAILED', "SWFUpload is taking a long time to load or the load has failed.  Please make sure that the Flash Plugin is enabled and that a working version of the Adobe Flash Player is installed.");
-define('_AD_FILEMANAGER_SWF_INSTALL_FLASH', "We're sorry.  SWFUpload could not load.  You may need to install or upgrade Flash Player.<br />
-Visit the <a href='http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash'>Adobe website</a> to get the Flash Player.");
+define("_AD_FILEMANAGER_SWF_UPLOAD_QUEUE","Envios de arquivo");
+define("_AD_FILEMANAGER_SWF_UPLOAD_CNACEL","Cancelar todos os envios de arquivos");
+define("_AD_FILEMANAGER_SWF_COULD_NOT_LOAD","Lamento, mas não foi possível carregar o SWFUpload. Você precisa habilitar o JavaScript para desfrutar do SWFUpload.");
+define("_AD_FILEMANAGER_SWF_LOADING","SWFUpload está carregando. Aguarde um momento");
+define("_AD_FILEMANAGER_SWF_LOAD_HAS_FAILED","SWFUpload está levando muito tempo para carregar ou a carga falhou. Por favor, certifique-se de que o Flash Plugin é habilitado e que uma versão de trabalho do Adobe Flash Player é instalado");
+define("_AD_FILEMANAGER_SWF_INSTALL_FLASH","Lamento, mas não foi possível carregar o SWFUpload. Pode ser necessário instalar ou atualizar o Flash Player. <br /> Visite o portal da <a href='http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash' rel='nofollow' target='_tab'>Adobe</a> para obter a última versão do Flash Player.");
 
 // --------------------------------------------------------
 // FFMPEG
 // --------------------------------------------------------
-define('_AD_FILEMANAGER_ACTION_CONVERT', "Convert to FLV");
-define('_AD_FILEMANAGER_ACTION_CAPTURE', "Make Capture Image");
+define("_AD_FILEMANAGER_ACTION_CONVERT","Converter para FLV");
+define("_AD_FILEMANAGER_ACTION_CAPTURE","Capturar imagem");
 
 ?>
