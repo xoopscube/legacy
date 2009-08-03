d9d0fc127ff6386b12ea453272960842ae693aba
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-08-03 04:50:01 +0000

    Profile Module: updating the Portuguese language files.
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@340 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/portuguese/modules/profile/language/portuguese/main.php
+++ extras/extra_languages/portuguese/modules/profile/language/portuguese/main.php
@@ -1,44 +1,50 @@
 <?php
-define("_MD_PROFILE_LANG_FIELD_TIP1","String - allows a series of characters to be stored.");
-define("_MD_PROFILE_LANG_FIELD_TIP2","Text - textarea with editor.");
-define("_MD_PROFILE_LANG_FIELD_TIP3","Int (Integers) - input value containing only decimal digits.");
-define("_MD_PROFILE_LANG_FIELD_TIP4","Date - as named it can specify a date.");
-define("_MD_PROFILE_LANG_FIELD_TIP5","Checkbox - allows a selection 0 or 1, true or false.");
-define("_MD_PROFILE_LANG_FIELD_TIP6","Selectbox - allows multiple options, see the example.");
-define("_MD_PROFILE_LANG_UNAME","User Name");
-define("_MD_PROFILE_ERROR_CONTENT_IS_NOT_FOUND","Content not found");
-define("_MD_PROFILE_ERROR_DBUPDATE_FAILED","Failed to update the database");
-define("_MD_PROFILE_ERROR_EMAIL","Incorrect email address value.");
-define("_MD_PROFILE_ERROR_EXTENSION","Uploaded file's extension does not match any entry in the allowed list.");
-define("_MD_PROFILE_ERROR_INTRANGE","Incorrect input on {0}.");
-define("_MD_PROFILE_ERROR_MAX","Input {0} with {1} or less numeric value.");
-define("_MD_PROFILE_ERROR_MAXLENGTH","Input {0} with {1} or less characters.");
-define("_MD_PROFILE_ERROR_MIN","Input {0} with {1} or more numeric value.");
-define("_MD_PROFILE_ERROR_MINLENGTH","Input {0} with {1} or more characters.");
-define("_MD_PROFILE_ERROR_OBJECTEXIST","Incorrect input on {0}.");
-define("_MD_PROFILE_ERROR_REQUIRED","{0} is required.");
-define("_MD_PROFILE_LANG_ACCESS","Access groups");
-define("_MD_PROFILE_LANG_ADD_A_NEW_DATA","Add a New Data");
-define("_MD_PROFILE_LANG_ADD_A_NEW_DEFINITIONS","Add a New Field");
-define("_MD_PROFILE_LANG_CANCEL","Cancel");
-define("_MD_PROFILE_LANG_CONTROL","Action");
-define("_MD_PROFILE_LANG_DATA_EDIT","Edit profile");
-define("_MD_PROFILE_LANG_DATA_DELETE","Delete profile data");
-define("_MD_PROFILE_LANG_DEFINITIONS_DELETE", "Delete Form Field");
-define("_MD_PROFILE_LANG_DEFINITIONS_EDIT","Edit fields");
-define("_MD_PROFILE_LANG_DESCRIPTION","Description");
-define("_MD_PROFILE_LANG_FIELD_ID","Field ID");
-define("_MD_PROFILE_LANG_FIELD_NAME","Field form name");
-define("_MD_PROFILE_LANG_FIELD_NAME_DESC","This is used only internally!");
-define("_MD_PROFILE_LANG_LABEL","Label");
-define("_MD_PROFILE_LANG_OPTIONS","Options");
-define("_MD_PROFILE_LANG_REQUIRED","Required?");
-define("_MD_PROFILE_LANG_SHOW_FORM","Display");
-define("_MD_PROFILE_LANG_SHOW_FORM_DESC","Check this option to display the field on User profile");
-define("_MD_PROFILE_LANG_TYPE","Type");
-define("_MD_PROFILE_LANG_UID","User ID");
-define("_MD_PROFILE_LANG_VALIDATION","Validation");
-define("_MD_PROFILE_LANG_WEIGHT","Order");
-define("_MD_PROFILE_MESSAGE_CONFIRM_DELETE","Do you really want to delete?");
-define("_MD_PROFILE_DESC_OPTIONS", "Separate options with a pipe '|' without extra spaces. Example: tea|coffee|green tea");
+// ******************************************************************** //
+// ** XOOPS Cube Legacy - Profile Module - Portuguese
+// ** Por Mikhail Miguel <mikhail@underpop.com> - http://xoopscube.com.br/
+// ** $Id$
+// **	License http://creativecommons.org/licenses/by/2.5/br/
+// ******************************************************************** //
+define("_MD_PROFILE_DESC_OPTIONS","Opções separadas com barras verticais (<i>pipes</i>), sem espaços extras. Exemplo: café|chá verde|guaraná|ritalina");
+define("_MD_PROFILE_ERROR_CONTENT_IS_NOT_FOUND","Conteúdo não encontrado");
+define("_MD_PROFILE_ERROR_DBUPDATE_FAILED","Houve uma falha ao tentar atualizar o banco de dados.");
+define("_MD_PROFILE_ERROR_EMAIL","Foi escrito um endereço de e-mail considerado incorreto pelo sistema.");
+define("_MD_PROFILE_ERROR_EXTENSION","Foi enviado um tipo de arquivo cuja extensão não confere com nenhuma das permitidas pelo sistema.");
+define("_MD_PROFILE_ERROR_INTRANGE","Entrada incorreta em {0}.");
+define("_MD_PROFILE_ERROR_MAX","Entrada {0} com {1} ou menos o valor numérico");
+define("_MD_PROFILE_ERROR_MAXLENGTH","Entrada {0} com  {1} caracteres ou menos");
+define("_MD_PROFILE_ERROR_MIN","Entrada {0} com {1} ou com maior valor numérico");
+define("_MD_PROFILE_ERROR_MINLENGTH","Entrada {0} com  {1} ou mais personagens");
+define("_MD_PROFILE_ERROR_OBJECTEXIST","Entrada incorreta em {0}.");
+define("_MD_PROFILE_ERROR_REQUIRED","{0} é um valor necessário.");
+define("_MD_PROFILE_LANG_ACCESS","Grupos de acesso ");
+define("_MD_PROFILE_LANG_ADD_A_NEW_DATA","Incluir um novo dado");
+define("_MD_PROFILE_LANG_ADD_A_NEW_DEFINITIONS","Incluir um novo campo");
+define("_MD_PROFILE_LANG_CANCEL","Cancelar");
+define("_MD_PROFILE_LANG_CONTROL","Ação");
+define("_MD_PROFILE_LANG_DATA_DELETE","Remover dados de perfil");
+define("_MD_PROFILE_LANG_DATA_EDIT","Editar perfil");
+define("_MD_PROFILE_LANG_DEFINITIONS_DELETE","Remover campo do formulário");
+define("_MD_PROFILE_LANG_DEFINITIONS_EDIT","Editar campos");
+define("_MD_PROFILE_LANG_DESCRIPTION","Descrição");
+define("_MD_PROFILE_LANG_FIELD_ID","ID do campo");
+define("_MD_PROFILE_LANG_FIELD_NAME","Nome do campo do formulário");
+define("_MD_PROFILE_LANG_FIELD_NAME_DESC","Isto é utilizado apenas internamente");
+define("_MD_PROFILE_LANG_FIELD_TIP1","<i>String</i> (Seqüência) - permite uma série de caracteres a ser armazenado");
+define("_MD_PROFILE_LANG_FIELD_TIP2","Texto - textarea com editor");
+define("_MD_PROFILE_LANG_FIELD_TIP3","Int (inteiros) - valor de entrada que contenha apenas dígitos decimais");
+define("_MD_PROFILE_LANG_FIELD_TIP4","Data - assim como o próprio nome diz, pode especificar uma data");
+define("_MD_PROFILE_LANG_FIELD_TIP5","Checkbox - permite uma selecção 0 ou 1, verdadeiro ou falso.");
+define("_MD_PROFILE_LANG_FIELD_TIP6","Selectbox - permite múltiplas opções, veja o exemplo.");
+define("_MD_PROFILE_LANG_LABEL","Título");
+define("_MD_PROFILE_LANG_OPTIONS","Opções");
+define("_MD_PROFILE_LANG_REQUIRED","Necessário?");
+define("_MD_PROFILE_LANG_SHOW_FORM","Mostrar");
+define("_MD_PROFILE_LANG_SHOW_FORM_DESC","Marque esta opção para mostrar o campo na página do perfil do associado");
+define("_MD_PROFILE_LANG_TYPE","Tipo");
+define("_MD_PROFILE_LANG_UID","N°");
+define("_MD_PROFILE_LANG_UNAME","Codinome");
+define("_MD_PROFILE_LANG_VALIDATION","Validação");
+define("_MD_PROFILE_LANG_WEIGHT","Ordem");
+define("_MD_PROFILE_MESSAGE_CONFIRM_DELETE","Você realmente deseja remover isto?");
 ?>
--- extras/extra_languages/pt_utf8/modules/profile/language/pt_utf8/admin.php
+++ extras/extra_languages/pt_utf8/modules/profile/language/pt_utf8/admin.php
@@ -1,5 +1,14 @@
 <?php
-define("_AD_PROFILE_LANG_DEFINITIONS_DELETE","Remove field");
-define("_AD_PROFILE_LANG_DEFINITIONS_EDIT","Edit field");
-define("_AD_PROFILE_LANG_DEFINITIONS_VIEW","Display field");
+// ******************************************************************** //
+// ** XOOPS Cube Legacy - Profile Module - Portuguese
+// ** Por Mikhail Miguel <mikhail@underpop.com> - http://xoopscube.com.br/
+// ** $Id$
+// **	License http://creativecommons.org/licenses/by/2.5/br/
+// ******************************************************************** //
+define("_AD_PROFILE_LANG_DEFINITIONS_DELETE","Remover campo");
+define("_AD_PROFILE_LANG_DEFINITIONS_EDIT","Editar campo");
+define("_AD_PROFILE_LANG_DEFINITIONS_VIEW","Mostrar campo");
+define("_AD_PROFILE_TIPS_DATA_DOWNLOAD", "Os dados exportados em CSV são ordenados pelo número do cadastro do associado (<i>user_id</i>).");
+define("_AD_PROFILE_DATA_NUM", "%d associados no banco de dados.");
+define("_AD_PROFILE_DATA_DOWNLOAD_DO", "Exportar em CSV");
 ?>
