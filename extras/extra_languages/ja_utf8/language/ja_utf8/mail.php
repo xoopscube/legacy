1515900a3f42ea93bc717b0042948745a0f12da6
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-11-01 11:56:41 +0000

    extra language
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@369 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ extras/extra_languages/ja_utf8/language/ja_utf8/mail.php
@@ -0,0 +1,10 @@
+<?php
+define("_MAIL_MSGBODY", "メッセージ本文がありません");
+define("_MAIL_FAILOPTPL", "テンプレートファイルの読み込みに失敗しました");
+define("_MAIL_FNAMENG", "「送信者」を記入してください");
+define("_MAIL_FEMAILNG", "「送信者メールアドレス」を記入してください");
+define("_MAIL_SENDMAILNG", "%sさん宛にメールを送信できませんでした");
+define("_MAIL_MAILGOOD", "%sさん宛にメールを送信しました");
+define("_MAIL_SENDPMNG", "%sさん宛にプライベートメッセージを送信できませんでした");
+define("_MAIL_PMGOOD", "%sさん宛にプライベートメッセージを送信しました");
+?>
---
+++ extras/extra_languages/ja_utf8/modules/pm/language/ja_utf8/main.php
@@ -0,0 +1,42 @@
+<?php
+
+define('_MD_PM_ERROR_ACCESS', "アクセスエラーです");
+define('_MD_PM_ERROR_MAXLENGTH', "{0}は半角{1}文字以内で入力して下さい");
+define('_MD_PM_ERROR_MESSAGE_SEND', "メッセージの送信に失敗しました");
+define('_MD_PM_ERROR_OBJECTEXIST', "{0}の入力値が不正です");
+define('_MD_PM_ERROR_PLZTRYAGAIN', "ユーザ名を確認しもう一度やり直してください。");
+define('_MD_PM_ERROR_REQUIRED', "{0}は必ず入力してください");
+define('_MD_PM_ERROR_USERNOEXIST', "選択したユーザは存在しません。");
+define('_MD_PM_ERROR_USERWROTE', "%s さんは書きました：");
+define('_MD_PM_LANG_PREVIOUS', "前のメッセージ");
+define('_MD_PM_LANG_SEND', "送信");
+define('_MD_PM_LANG_SENTC', "送信日時： ");
+define('_MD_PM_LANG_CANCELSEND', "キャンセル");
+define('_MD_PM_LANG_CLEAR', "クリア");
+define('_MD_PM_LANG_DATE', "送信日時");
+define('_MD_PM_LANG_DELETE', "削除");
+define('_MD_PM_LANG_FROM', "送信者");
+define('_MD_PM_LANG_FROMC', "送信者： ");
+define('_MD_PM_LANG_INBOX', "受信箱");
+define('_MD_PM_LANG_MESSAGE', "メッセージ");
+define('_MD_PM_LANG_MESSAGE_DETAIL', "メッセージの詳細");
+define('_MD_PM_LANG_MESSAGEC', "メッセージ：");
+define('_MD_PM_LANG_MSG_ID', "メッセージID");
+define('_MD_PM_LANG_NEXT', "次のメッセージ");
+define('_MD_PM_LANG_NOTREAD', "未読");
+define('_MD_PM_LANG_ORCLOSEWINDOW', "このウィンドウを閉じる");
+define('_MD_PM_LANG_PROFILE', "プロフィール");
+define('_MD_PM_LANG_REPLY', "返信");
+define('_MD_PM_LANG_SUBJECT', "件名");
+define('_MD_PM_LANG_SUBJECTC', "件名： ");
+define('_MD_PM_LANG_SUBMIT', "送信");
+define('_MD_PM_LANG_TO', "宛先：");
+define('_MD_PM_MESSAGE_CLICKHERE', "プライベートメッセージの受信箱を表示する");
+define('_MD_PM_MESSAGE_CONFIRM_DELETE_PM', "以下のプライベートメッセージを本当に削除しますか？");
+define('_MD_PM_MESSAGE_DELETED', "メッセージを削除しました。");
+define('_MD_PM_MESSAGE_PLZREG', "プライベートメッセージを使用するにはユーザ登録が必要です。");
+define('_MD_PM_MESSAGE_POSTED', "メッセージを送信しました。");
+define('_MD_PM_MESSAGE_SORRY', "プライベートメッセージを使用するにはユーザ登録が必要です。");
+define('_MD_PM_MESSAGE_YOUDONTHAVE', "プライベートメッセージはありません。");
+
+?>
