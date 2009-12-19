71e0d0ad7d59faf955fa4535be92e6673fb168b8
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2009-12-19 13:21:32 +0000

    encode fr_utf8 filemanager
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@380 704cf05f-ae62-4b0e-a484-234ee0250e75

---
+++ extras/extra_languages/fr_utf8/modules/fileManager/language/fr_utf8/admin.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Filemaneger
+ * (C)2007-2009 BeaBo Japan by Hiroki Seike
+ * http://beabo.net/
+ **/
+
+// --------------------------------------------------------
+// Main
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_MAIN_DSC', "List of file uploads. When you click a folder to move to the folder. The big picture and video preview is available.");
+define('_AD_FILEMANAGER_PATH_HOME', "Accueil");
+define('_AD_FILEMANAGER_TYPE', "Type");
+define('_AD_FILEMANAGER_EDIT', "Ajouter");
+define('_AD_FILEMANAGER_DEL', "Supprimer");
+define('_AD_FILEMANAGER_PARENT', "Retour");
+define('_AD_FILEMANAGER_RETURN', "Retourner à la liste");
+define('_AD_FILEMANAGER_ACTION_DELETE', "&nbsp;Supprimer&nbsp;");
+define('_AD_FILEMANAGER_ACTION_DEFULT', "&nbsp;Ajouter&nbsp; ");
+define('_AD_FILEMANAGER_ACTION_SUBMIT', "&nbsp;Envoyer&nbsp;");
+define('_AD_FILEMANAGER_FILE_TOTAL', "Total");
+
+// --------------------------------------------------------
+// Error
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_ERROR_REQUIRED', "{0} ist requis.");
+define('_AD_FILEMANAGER_ERROR_PERMISSION', "Vous n'avez pas les permissions d'accès requises.");
+define('_AD_FILEMANAGER_ERROR_DELETE_FOR_PERMISSION', "You do not have access %s to the file manager can not be removed from.");
+define('_AD_FILEMANAGER_NOTFOUND', "Fichier introuvable.");
+
+// --------------------------------------------------------
+// Uploads
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_PREVIEW', "Prévisualiser");
+define('_AD_FILEMANAGER_FILENAME', "Fichier");
+define('_AD_FILEMANAGER_SIZE', "Taille");
+define('_AD_FILEMANAGER_DATE', "Date");
+define('_AD_FILEMANAGER_UPLOAD', "Télécharger");
+define('_AD_FILEMANAGER_UPLOAD_DSC', "Click upload to select for your computer's files and start the upload.");
+define('_AD_FILEMANAGER_UPLOAD_NOTACCESS',  "%s is not upload. FTP software, please change the permissions.");
+define('_AD_FILEMANAGER_NOTFOUNDURL', "Upload Paht is not fuound");
+define('_AD_FILEMANAGER_CONFIRMMSSAGE', "You can upload up to max %s .");
+define('_AD_FILEMANAGER_FOLDER_ADD', "Ajouter Dossier");
+
+// --------------------------------------------------------
+// Folder
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_FOLDER', "Dossier");
+define('_AD_FILEMANAGER_FOLDERNAME', "Nom du nouveau dossier");
+define('_AD_FILEMANAGER_ERROR_FOLDERNAME', "Folder name is not correct.<br />Characters can be used in the name of the folder, and alphanumeric characters -_.");
+define('_AD_FILEMANAGER_ERROR_PATH', "older name is not right. Please confirm a folder name.");
+define('_AD_FILEMANAGER_ADD', "Ajouter Folder");
+define('_AD_FILEMANAGER_ADDFOLDER', "Ajouter Folder");
+define('_AD_FILEMANAGER_ADDFOLDER_DSC', "Créer un nouveau dossier dans le repértoire XOOPS /uploads.");
+define('_AD_FILEMANAGER_ADDFOLDER_SUCCESS', "Ajouter Dossier.");
+define('_AD_FILEMANAGER_ADDFOLDER_ERROR', "Impossible d'ajouter le dossier.");
+define('_AD_FILEMANAGER_ADDFOLDER_CONFIRMMSSAGE', "Créer un dossier sous %s <br />Ajoutez un nom pour le nouveau dossier.");
+define('_AD_FILEMANAGER_DELET', "Supprimer");
+define('_AD_FILEMANAGER_DELFOLDER', "Supprimer Dossier");
+define('_AD_FILEMANAGER_DELFOLDER_DSC', "Suprimer le dossier spécifié.");
+define('_AD_FILEMANAGER_DELFOLDER_CONFIRMMSSAGE', "Supprimer le dossier %s");
+define('_AD_FILEMANAGER_DELFOLDER_FILE_EXISTS', "%s for the file to the folder that can not be deleted. To verify the contents of a folder, please.");
+define('_AD_FILEMANAGER_DELFOLDER_SUCCESS', "Dossier supprimé.");
+define('_AD_FILEMANAGER_DELFOLDER_ERROR', "I can not delete the folder. Not specify an empty folder, or is not appropriate permissions.");
+define('_AD_FILEMANAGER_DELFOLDER_ISDIR', "Folder %s is can not be removed from the file manager.");
+define('_AD_FILEMANAGER_DELFOLDER_NOTACCESS', "Folder %s is can not be removed from the file manager. FTP software, please change the permissions.");
+define('_AD_FILEMANAGER_FILECOUNT', "Fichiers");
+
+// --------------------------------------------------------
+// Setting check
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_CHECK_NG', "Library file is not found.Please upload files.<br />");
+define('_AD_FILEMANAGER_CHECK_OK', "The setting of the library file is completed.");
+define('_AD_FILEMANAGER_CHECK', "Setting Check");
+define('_AD_FILEMANAGER_CHECK_DSC_1', "Using SWFUpload");
+define('_AD_FILEMANAGER_CHECK_DSC_2', "SWFUpload to upload for public use is not recommended.<br />The htaccess set to the location of the library, please set up with restricted access.");
+define('_AD_FILEMANAGER_HTACCESS_DSC_1', "You can choose your settings and click the form");
+define('_AD_FILEMANAGER_HTACCESS_DSC_2', "That allow access your current IP address.(Sample composition is automatic. Please change to suit your server environment.)");
+define('_AD_FILEMANAGER_HTACCESS_PATH', "htaccess file to install path");
+
+// --------------------------------------------------------
+// SWFUpload
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_SWF_UPLOAD_QUEUE', "Uploads");
+define('_AD_FILEMANAGER_SWF_UPLOAD_CNACEL', "Cancel All Uploads");
+define('_AD_FILEMANAGER_SWF_COULD_NOT_LOAD', "We're sorry.  SWFUpload could not load.  You must have JavaScript enabled to enjoy SWFUpload.");
+define('_AD_FILEMANAGER_SWF_LOADING', "SWFUpload is loading. Please wait a moment...");
+define('_AD_FILEMANAGER_SWF_LOAD_HAS_FAILED', "SWFUpload is taking a long time to load or the load has failed.  Please make sure that the Flash Plugin is enabled and that a working version of the Adobe Flash Player is installed.");
+define('_AD_FILEMANAGER_SWF_INSTALL_FLASH', "We're sorry.  SWFUpload could not load.  You may need to install or upgrade Flash Player.<br />
+Visit the <a href='http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash'>Adobe website</a> to get the Flash Player.");
+
+// --------------------------------------------------------
+// FFMPEG
+// --------------------------------------------------------
+define('_AD_FILEMANAGER_ACTION_CONVERT', "Convert to FLV");
+define('_AD_FILEMANAGER_ACTION_CAPTURE', "Make Capture Image");
+
+?>
