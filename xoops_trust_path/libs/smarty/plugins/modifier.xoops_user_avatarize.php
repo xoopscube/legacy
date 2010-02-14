97c24b4dc52718d5ceb41add739deb0285371160
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-02-14 08:48:07 +0000

    extend xoops_user_avatarize smarty plugin to create html tag
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@453 704cf05f-ae62-4b0e-a484-234ee0250e75

--- xoops_trust_path/libs/smarty/plugins/modifier.xoops_user_avatarize.php
+++ xoops_trust_path/libs/smarty/plugins/modifier.xoops_user_avatarize.php
@@ -17,17 +17,32 @@
  * Input:    uid: user id
  * -------------------------------------------------------------
  */
-function smarty_modifier_xoops_user_avatarize($uid)
+function smarty_modifier_xoops_user_avatarize($uid, $tag=false, $url=null)
 {
 	$handler =& xoops_gethandler('user');
 	$user =& $handler->get(intval($uid));
-	if (is_object($user) && $user->isActive() && ($user->get('user_avatar') != "blank.gif")) {
-		if (file_exists(XOOPS_UPLOAD_PATH . "/" . $user->get('user_avatar'))) {
-			return XOOPS_UPLOAD_URL . "/" . $user->getShow('user_avatar');
-		}
+	if (is_object($user) && $user->isActive() && ($user->get('user_avatar') != "blank.gif") && file_exists(XOOPS_UPLOAD_PATH . "/" . $user->get('user_avatar'))) {
+		$src = XOOPS_UPLOAD_URL . "/" . $user->getShow('user_avatar');
+		$path = XOOPS_UPLOAD_PATH . "/" . $user->getShow('user_avatar');
+	}
+	else{
+		$src = XOOPS_URL . "/modules/user/images/no_avatar.gif";
+		$path = XOOPS_UPLOAD_PATH . "/modules/user/images/no_avatar.gif";
 	}
 
-	return XOOPS_URL . "/modules/user/images/no_avatar.gif";
+	if($tag===true){
+		list($width, $height, $type, $attr) = getimagesize($path);
+		$imageTag = sprintf('<img src="%s" width="%d" height="%d" alt="%s" />', $src, $width, $height, $user->get('uname'));
+		if(is_null($url)){
+			return $imageTag;
+		}
+		else{
+			return '<a href="'.$url.'">'.$imageTag.'</a>';
+		}
+	}
+	else{
+		return $src;
+	}
 }
 
 ?>
