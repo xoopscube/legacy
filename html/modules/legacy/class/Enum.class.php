f56ce844a9881cafb7eccf2dc3035c60b0d98b3f
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-03-07 12:52:26 +0000

    add Legacy_Status Enum
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@507 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/legacy/class/Enum.class.php
+++ html/modules/legacy/class/Enum.class.php
@@ -8,25 +8,35 @@
 
 if(!defined('XOOPS_ROOT_PATH'))
 {
-    exit;
+	exit;
 }
 
+/**
+ * Legacy_Status
+**/
+interface Legacy_Status
+{
+	const DELETED = 0;
+	const REJECTED = 2;
+	const PROGRESS = 5;
+	const PUBLISHED = 9;
+}
 /**
  * Legacy_ProgressStatus
 **/
 interface Legacy_ProgressStatus
 {
-    const DELETED = 0;
-    const REJECTED = 2;
-    const PROGRESS = 5;
-    const FINISHED = 9;
+	const DELETED = 0;
+	const REJECTED = 2;
+	const PROGRESS = 5;
+	const FINISHED = 9;
 }
 
 interface Legacy_TextareaEditorEnum
 {
-    const BBCODE = 0;   //default
-    const HTML = 1;
-    const NONE = 2;
+	const BBCODE = 0;	//default
+	const HTML = 1;
+	const NONE = 2;
 }
 
 ?>
