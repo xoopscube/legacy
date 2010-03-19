2041126e816337ce81f035c333389694bd64737a
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-03-19 10:30:21 +0000

    Add function to get data from profile field.
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@525 704cf05f-ae62-4b0e-a484-234ee0250e75

--- xoops_trust_path/libs/smarty/plugins/modifier.xoops_user.php
+++ xoops_trust_path/libs/smarty/plugins/modifier.xoops_user.php
@@ -14,16 +14,29 @@
  * Type:     modifier
  * Name:     xoops_user
  * Purpose:  Adapter for XoopsUserObject::getVar with using $uid parameter.
- * Input:    uid: user id
- *           key: XoopsUserObject property name
+ * Input:    uid : user id
+ *           key : XoopsUserObject property name
+ *           flag: Enum(Profile_ActionType) 
+ *             If you set 0, you can get raw value.
+ *             If you set 2 or nothing, you can get escaped value.
  * -------------------------------------------------------------
  */
-function smarty_modifier_xoops_user($uid, $key)
+function smarty_modifier_xoops_user($uid, $key, $flag=0)
 {
-	$handler=&xoops_gethandler('member');
-	$user=&$handler->getUser(intval($uid));
-	if(is_object($user)&&$user->isActive()) {
-		return $user->getShow($key);
+	$flag = isset($flag) ? $flag : 2;
+	if(in_array($key, Profile_DefinitionsHandler::getReservedNameList())){
+		$handler=&xoops_gethandler('member');
+		$user=&$handler->getUser(intval($uid));
+		if(is_object($user) && $user->isActive()) {
+			return ($flag==2) ? $user->getShow($key) : $user->get($key);
+		}
+	}
+	else{
+		$handler =& Legacy_Utils::getModuleHandler('data', 'profile');
+		$user =& $handler->get($uid);
+		if(is_object($user) && $user->isActive()) {
+			return $user->showField($key, $flag);
+		}
 	}
 
 	return null;
