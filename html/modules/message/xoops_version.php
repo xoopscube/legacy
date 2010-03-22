14707340a4ba061d102601fb0e1968d56ed9e2eb
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-03-22 13:20:10 +0000

    update module_icon setting in xoops_version.php
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@551 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/message/xoops_version.php
+++ html/modules/message/xoops_version.php
@@ -15,7 +15,7 @@ $modversion['author'] = 'Marijuana';
 $modversion['credits'] = "XOOPS Cube Project";
 $modversion['help'] = "help.html";
 $modversion['license'] = "MIT LICENSE";
-$modversion['image']       = file_exists( $mydirpath.'/images/module_icon.png' ) ? 'images/module_icon.png' : 'module_icon.php' ;
+$modversion['image']	   = 'images/module_icon.png';
 $modversion['mcl_update'] = 'message';
 
 $modversion['cube_style'] = true;
@@ -50,47 +50,47 @@ $modversion['sub'][] = array('name' => _MI_MESSAGE_SUB_SETTINGS, 'url' => 'index
 
 $modversion['hasAdmin'] = 1;
 
-$modversion['config'][0]['name']        = 'pagenum';
-$modversion['config'][0]['title']       = '_MI_MESSAGE_PAGENUM';
+$modversion['config'][0]['name']		= 'pagenum';
+$modversion['config'][0]['title']		= '_MI_MESSAGE_PAGENUM';
 $modversion['config'][0]['description'] = '_MI_MESSAGE_PAGENUM_DESC';
-$modversion['config'][0]['formtype']    = 'textbox';
-$modversion['config'][0]['valuetype']   = 'int';
-$modversion['config'][0]['default']     = '15';
+$modversion['config'][0]['formtype']	= 'textbox';
+$modversion['config'][0]['valuetype']	= 'int';
+$modversion['config'][0]['default'] 	= '15';
 
-$modversion['config'][1]['name']        = 'savedays';
-$modversion['config'][1]['title']       = '_MI_MESSAGE_SAVEDAYS';
+$modversion['config'][1]['name']		= 'savedays';
+$modversion['config'][1]['title']		= '_MI_MESSAGE_SAVEDAYS';
 $modversion['config'][1]['description'] = '_MI_MESSAGE_SAVEDAYS_DESC';
-$modversion['config'][1]['formtype']    = 'textbox';
-$modversion['config'][1]['valuetype']   = 'int';
-$modversion['config'][1]['default']     = '90';
+$modversion['config'][1]['formtype']	= 'textbox';
+$modversion['config'][1]['valuetype']	= 'int';
+$modversion['config'][1]['default'] 	= '90';
 
-$modversion['config'][2]['name']        = 'newalert';
-$modversion['config'][2]['title']       = '_MI_MESSAGE_NEWALERT';
+$modversion['config'][2]['name']		= 'newalert';
+$modversion['config'][2]['title']		= '_MI_MESSAGE_NEWALERT';
 $modversion['config'][2]['description'] = '_MI_MESSAGE_NEWALERT_DESC';
-$modversion['config'][2]['formtype']    = 'yesno';
-$modversion['config'][2]['valuetype']   = 'int';
-$modversion['config'][2]['default']     = '1';
+$modversion['config'][2]['formtype']	= 'yesno';
+$modversion['config'][2]['valuetype']	= 'int';
+$modversion['config'][2]['default'] 	= '1';
 
-$modversion['config'][3]['name']        = 'userinfo';
-$modversion['config'][3]['title']       = '_MI_MESSAGE_USERINFO';
+$modversion['config'][3]['name']		= 'userinfo';
+$modversion['config'][3]['title']		= '_MI_MESSAGE_USERINFO';
 $modversion['config'][3]['description'] = '_MI_MESSAGE_USERINFO_DESC';
-$modversion['config'][3]['formtype']    = 'yesno';
-$modversion['config'][3]['valuetype']   = 'int';
-$modversion['config'][3]['default']     = '1';
+$modversion['config'][3]['formtype']	= 'yesno';
+$modversion['config'][3]['valuetype']	= 'int';
+$modversion['config'][3]['default'] 	= '1';
 
-$modversion['config'][4]['name']        = 'dletype';
-$modversion['config'][4]['title']       = '_MI_MESSAGE_DELTYPE';
+$modversion['config'][4]['name']		= 'dletype';
+$modversion['config'][4]['title']		= '_MI_MESSAGE_DELTYPE';
 $modversion['config'][4]['description'] = '_MI_MESSAGE_DELTYPE_DESC';
-$modversion['config'][4]['formtype']    = 'yesno';
-$modversion['config'][4]['valuetype']   = 'int';
-$modversion['config'][4]['default']     = '1';
+$modversion['config'][4]['formtype']	= 'yesno';
+$modversion['config'][4]['valuetype']	= 'int';
+$modversion['config'][4]['default'] 	= '1';
 
-$modversion['blocks'][0]['file']        = 'message_block.class.php';
-$modversion['blocks'][0]['name']        = _MI_MESSAGE_BLOCK_NAME;
+$modversion['blocks'][0]['file']		= 'message_block.class.php';
+$modversion['blocks'][0]['name']		= _MI_MESSAGE_BLOCK_NAME;
 $modversion['blocks'][0]['description'] = '';
-$modversion['blocks'][0]['show_func']   = '';
-$modversion['blocks'][0]['class']       = 'Block';
-$modversion['blocks'][0]['template']    = 'message_block_template.html';
-$modversion['blocks'][0]['visible']     = '1';
-$modversion['blocks'][0]['func_num']    = 1;
+$modversion['blocks'][0]['show_func']	= '';
+$modversion['blocks'][0]['class']		= 'Block';
+$modversion['blocks'][0]['template']	= 'message_block_template.html';
+$modversion['blocks'][0]['visible'] 	= '1';
+$modversion['blocks'][0]['func_num']	= 1;
 ?>
