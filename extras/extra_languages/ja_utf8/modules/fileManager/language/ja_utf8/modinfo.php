ec5e92cbe6d5958987f59e42410268ff664b57af
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-03-31 13:02:40 +0000

    Fix Bug: #2979992 - filemanager's ja_utf8 files are saved as EUC
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@588 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/ja_utf8/modules/fileManager/language/ja_utf8/modinfo.php
+++ extras/extra_languages/ja_utf8/modules/fileManager/language/ja_utf8/modinfo.php
@@ -5,53 +5,53 @@
  * http://beabo.net/
  **/
 
-define('_MI_FILEMANAGER_NAME', "�ե�����ޥ͡����㡼");
-define('_MI_FILEMANAGER_DESC', "uploads�ե�����Υե���������ȥե�����Υ��åץ����ɤ��ñ��");
-define('_MI_FILEMANAGER_UPDATE', '���åץǡ���');
+define('_MI_FILEMANAGER_NAME', "ファイルマネージャー");
+define('_MI_FILEMANAGER_DESC', "uploadsフォルダのファイル管理とファイルのアップロードを簡単に");
+define('_MI_FILEMANAGER_UPDATE', 'アップデート');
 
 // --------------------------------------------------------
 // Names of admin menu items
 // --------------------------------------------------------
-define('_MI_FILEMANAGER_MAIN', "�ե�����ꥹ��");
-define('_MI_FILEMANAGER_MAIN_DSC', "���åץ����ɥե�����Υե�����ꥹ��ɽ��");
-define('_MI_FILEMANAGER_UPLOAD', "���åץ�����");
-define('_MI_FILEMANAGER_UPLOAD_DSC', "�ե�����Υ��åץ����ɤ�Ԥ��ޤ�");
-define('_MI_FILEMANAGER_FOLDER', "�ե���������");
-define('_MI_FILEMANAGER_FOLDER_DSC', "�ե�������ɲá������Ԥ��ޤ���");
-define('_MI_FILEMANAGER_CHECK', "ư��Ķ��γ�ǧ");
-define('_MI_FILEMANAGER_CHECK_DSC', "�ե�����ޥ͡����㡼�����꤫��ư��Ķ�������å����ޤ���");
+define('_MI_FILEMANAGER_MAIN', "ファイルリスト");
+define('_MI_FILEMANAGER_MAIN_DSC', "アップロードフォルダのファイルリスト表示");
+define('_MI_FILEMANAGER_UPLOAD', "アップロード");
+define('_MI_FILEMANAGER_UPLOAD_DSC', "ファイルのアップロードを行います");
+define('_MI_FILEMANAGER_FOLDER', "フォルダの操作");
+define('_MI_FILEMANAGER_FOLDER_DSC', "フォルダの追加・削除を行います。");
+define('_MI_FILEMANAGER_CHECK', "動作環境の確認");
+define('_MI_FILEMANAGER_CHECK_DSC', "ファイルマネージャーの設定から動作環境をチェックします。");
 
 // --------------------------------------------------------
 // Preference Edit
 // --------------------------------------------------------
-define('_MI_FILEMANAGER_PATH',"���åץ����ɤ���ǥե���ȤΥѥ�");
-define('_MI_FILEMANAGER_PATH_DSC',"���åץ����ɤΥѥ�����ꤷ�ʤ����˥��åץ����ɤ���ѥ�̾����ꤷ�ޤ���<br />����URL/uploads/ ��ɸ��ˤʤ�ޤ���<br />�㡧temp �Ȼ��ꤷ����硢����URL/uploads/temp/ ��ɸ��Υ��åץ�������ˤʤ�ޤ��� ");
-define('_MI_FILEMANAGER_DIRHANDLE',"�ե���������򥵥ݡ���");
-define('_MI_FILEMANAGER_DIRHANDLE_DSC',"�֤Ϥ��פ����֤ȥե�����������ǽ�ˤ��ޤ����ե�������������Τϡ��ե�����Υ�����������777�ξ��Τߤ�ͭ���Ǥ���");
-define('_MI_FILEMANAGER_THUMBSIZE',"����ͥ��륵����");
-define('_MI_FILEMANAGER_THUMBSIZE_DSC',"�ե�����ꥹ��ɽ�����Υ���ͥ��륵��������ꤷ�ޤ���ñ�̤ϥԥ�����Ǥ���");
-define('_MI_FILEMANAGER_DEBUGON',"���åץ��������ΥǥХå��򥪥�");
-define('_MI_FILEMANAGER_DEBUGON_DSC',"SWF���åץ����ɤΥǥХå����ǽ�ˤ��ޤ����̾�Ϥ������ǻ��Ѥ��Ƥ���������");
-define('_MI_FILEMANAGER_XOOPSLOCK',"�����ƥ��������ɽ��");
-define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"�����ƥ�ǻȤäƤ��������ɽ�����ʤ������᡼���ޥ͡����㡼�����Х������饢�������ɽ�����ޤ���");
-define('_MI_FILEMANAGER_EXTENSIONS',"���åץ����ɲ�ǽ�ʥե�����γ�ĥ��");
-define('_MI_FILEMANAGER_EXTENSIONS_DSC',"���åץ����ɤ���Ĥ���ե�����γ�ĥ�Ҥ� '|' ���ڤ�ǻ��ꤷ�ޤ�����ĥ�Ҥϡ����ƾ�ʸ�������Ϥ��Ʋ�������<br />�ǥե���Ȥϡ�gif|jpg|jpeg|png|avi|mov|wmv|mp3|mp4|flv|doc|xls|ods|odt|pdf �Ǥ���");
+define('_MI_FILEMANAGER_PATH',"アップロードするデフォルトのパス");
+define('_MI_FILEMANAGER_PATH_DSC',"アップロードのパスを指定しない場合にアップロードするパス名を指定します。<br />設置URL/uploads/ が標準になります。<br />例：temp と指定した場合、設置URL/uploads/temp/ が標準のアップロード先になります。 ");
+define('_MI_FILEMANAGER_DIRHANDLE',"フォルダの操作をサポート");
+define('_MI_FILEMANAGER_DIRHANDLE_DSC',"「はい」を選ぶとフォルダの操作を可能にします。フォルダの操作が出来るのは、フォルダのアクセス権が777の場合のみに有効です。");
+define('_MI_FILEMANAGER_THUMBSIZE',"サムネイルサイズ");
+define('_MI_FILEMANAGER_THUMBSIZE_DSC',"ファイルリスト表示時のサムネイルサイズを指定します。単位はピクセルです。");
+define('_MI_FILEMANAGER_DEBUGON',"アップローダーのデバックをオン");
+define('_MI_FILEMANAGER_DEBUGON_DSC',"SWFアップロードのデバックを可能にします。通常はいいえで使用してください。");
+define('_MI_FILEMANAGER_XOOPSLOCK',"システム画像の非表示");
+define('_MI_FILEMANAGER_XOOPSLOCK_DSC',"システムで使っている画像を表示しない。イメージマネージャー・アバター・顔アイコンを表示しません。");
+define('_MI_FILEMANAGER_EXTENSIONS',"アップロード可能なファイルの拡張子");
+define('_MI_FILEMANAGER_EXTENSIONS_DSC',"アップロードを許可するファイルの拡張子を '|' 区切りで指定します。拡張子は、全て小文字で入力して下さい。<br />デフォルトは、gif|jpg|jpeg|png|avi|mov|wmv|mp3|mp4|flv|doc|xls|ods|odt|pdf です。");
 
 // ffmpeg Preference
-define('_MI_FILEMANAGER_FUSE',"��ffmpeg��ffmpeg�����Ѥ���");
-define('_MI_FILEMANAGER_FUSE_DSC',"ffmpeg�����Ѥ�����ϡ��Ϥ������򤷤Ʋ�������ffmpeg�ϡ������С�¦���б����Ƥ���ɬ�פ�����ޤ���<br />���Ȥ��Υ����С����б������Х��ʥ�����֤��뤫���ӥ�ɤ��Ʋ�������");
-define('_MI_FILEMANAGER_FPATH',"��ffmpeg�ۥ��ޥ�ɸ����ѥ�");
-define('_MI_FILEMANAGER_FPATH_DSC',"ffmpeg�μ¹ԥե�����إѥ����̤äƤ��ʤ����ϡ����֥ѥ�����ꤷ�ޤ���<br />(��: <tt>/usr/local/bin</tt><tt>:/usr/bin</tt>)");
-define('_MI_FILEMANAGER_FCAPTURE',"��ffmpeg�ۥ����꡼�󥷥�åȤλ���");
-define('_MI_FILEMANAGER_FCAPTURE_DSC',"ư��ե����뤫�饹���꡼�󥷥�åȤ򻣤���֤�ư�����Ƭ����λ��֡��áˤ���ꤷ�ޤ���");
-define('_MI_FILEMANAGER_FCONVERT',"��ffmpeg��FLV�Ѵ��κ��祵����");
-define('_MI_FILEMANAGER_FCONVERT_DSC',"ư��ե����뤫��FLV������ư����Ѵ��������Υ���������ꤷ�ޤ���ñ�̤�MB�Ǥ���");
-define('_MI_FILEMANAGER_MULTIUPLOAD',"�ޥ�����åץ������������Ѥ���");
-define('_MI_FILEMANAGER_MULTIUPLOAD_DSC',"ʣ���ե��������٤˥��åץ����ɤ�����ˡ�ȤҤȤĤΥե����뤴�Ȥ˥��åץ����ɤ�Ԥ�����������Ǥ��ޤ���");
-define('_MI_FILEMANAGER_MULTIUPLOAD_0',"�ޥ�����åץ������������Ѥ���");
-define('_MI_FILEMANAGER_MULTIUPLOAD_1',"�ե����뤴�Ȥ˥��åץ����ɤ���");
-define('_MI_FILEMANAGER_FMOVIEFILE',"��ffmpeg��FLV�Ѵ��Υե��������");
-define('_MI_FILEMANAGER_FMOVIEFILE_DSC',"FLV�Ѵ���ǽ��ư��ե�����γ�ĥ�Ҥ� '|' ���ڤ�ǻ��ꤷ�ޤ�����ĥ�Ҥϡ����ƾ�ʸ�������Ϥ��Ʋ�������<br />���Ȥ���ffmpeg�ˤ�ä��Ѵ������ե�����������ۤʤ�ޤ���<br />�ǥե���Ȥϡ�flv|avi|mwv|mov|mpg|qt|mov|3gp|3gp2|mp4 �Ǥ���");
+define('_MI_FILEMANAGER_FUSE',"【ffmpeg】ffmpegを利用する");
+define('_MI_FILEMANAGER_FUSE_DSC',"ffmpegを利用する場合は、はいを選択して下さい。ffmpegは、サーバー側が対応している必要があります。<br />お使いのサーバーに対応したバイナリを設置するか、ビルドして下さい。");
+define('_MI_FILEMANAGER_FPATH',"【ffmpeg】コマンド検索パス");
+define('_MI_FILEMANAGER_FPATH_DSC',"ffmpegの実行ファイルへパスが通っていない場合は、設置パスを指定します。<br />(例: <tt>/usr/local/bin</tt><tt>:/usr/bin</tt>)");
+define('_MI_FILEMANAGER_FCAPTURE',"【ffmpeg】スクリーンショットの時間");
+define('_MI_FILEMANAGER_FCAPTURE_DSC',"動画ファイルからスクリーンショットを撮る時間を動画の先頭からの時間（秒）を指定します。");
+define('_MI_FILEMANAGER_FCONVERT',"【ffmpeg】FLV変換の最大サイズ");
+define('_MI_FILEMANAGER_FCONVERT_DSC',"動画ファイルからFLV形式の動画へ変換する最大のサイズを指定します。単位はMBです。");
+define('_MI_FILEMANAGER_MULTIUPLOAD',"マルチアップローダーを利用する");
+define('_MI_FILEMANAGER_MULTIUPLOAD_DSC',"複数ファイルを一度にアップロードする方法とひとつのファイルごとにアップロードを行う方式を選択できます。");
+define('_MI_FILEMANAGER_MULTIUPLOAD_0',"マルチアップローダーを利用する");
+define('_MI_FILEMANAGER_MULTIUPLOAD_1',"ファイルごとにアップロードする");
+define('_MI_FILEMANAGER_FMOVIEFILE',"【ffmpeg】FLV変換のファイル形式");
+define('_MI_FILEMANAGER_FMOVIEFILE_DSC',"FLV変換可能な動画ファイルの拡張子を '|' 区切りで指定します。拡張子は、全て小文字で入力して下さい。<br />お使いのffmpegによって変換出来るファイル形式が異なります。<br />デフォルトは、flv|avi|mwv|mov|mpg|qt|mov|3gp|3gp2|mp4 です。");
 
 
 ?>
