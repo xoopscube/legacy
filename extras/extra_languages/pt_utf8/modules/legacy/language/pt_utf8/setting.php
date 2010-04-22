9bb22c11025a48a474418907fe663c27551f8c49
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-04-22 20:27:18 +0000

    Updated Portuguese language files
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@614 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/pt_utf8/modules/legacy/language/pt_utf8/setting.php
+++ extras/extra_languages/pt_utf8/modules/legacy/language/pt_utf8/setting.php
@@ -1,53 +1,97 @@
 <?php
-
-//%%%%%		TIME FORMAT SETTINGS   %%%%%
-if (!defined('_DATESTRING')) define("_DATESTRING","Y/n/j G:i:s");
-if (!defined('_MEDIUMDATESTRING')) define("_MEDIUMDATESTRING","Y/n/j G:i");
-if (!defined('_SHORTDATESTRING')) define("_SHORTDATESTRING","Y/n/j");
-define('_JSDATEPICKSTRING','yy-mm-dd');
-define('_PHPDATEPICKSTRING','Y-m-d');
-
+// Translation Info
+// $Id$
+// License http://creativecommons.org/licenses/by/2.5/br/
+// ############################################################### //
+// ## XOOPS Cube Legacy - Versão em Português
+// ############################################################### //
+// ## Por............: Mikhail Miguel
+// ## Website........: http://xoopscube.com.br
+// ## E-mail.........: mikhail@underpop.com
+// ## AOL............: mikhailmiguel
+// ## MSN............: mikhail.miguel@hotmail.com
+// ## Orkut..........: 15440532260129226492
+// ## Skype..........: mikhailmiguel
+// ## Yahoo!.........: mikhailmiguel@yahoo.com
+// ############################################################### //
+// *************************************************************** //
 /*
-The following characters are recognized in the format string:
-a - "am" or "pm"
-A - "AM" or "PM"
-d - day of the month, 2 digits with leading zeros; i.e. "01" to "31"
-D - day of the week, textual, 3 letters; i.e. "Fri"
-F - month, textual, long; i.e. "January"
-h - hour, 12-hour format; i.e. "01" to "12"
-H - hour, 24-hour format; i.e. "00" to "23"
-g - hour, 12-hour format without leading zeros; i.e. "1" to "12"
-G - hour, 24-hour format without leading zeros; i.e. "0" to "23"
-i - minutes; i.e. "00" to "59"
-j - day of the month without leading zeros; i.e. "1" to "31"
-l (lowercase 'L') - day of the week, textual, long; i.e. "Friday"
-L - boolean for whether it is a leap year; i.e. "0" or "1"
-m - month; i.e. "01" to "12"
-n - month without leading zeros; i.e. "1" to "12"
-M - month, textual, 3 letters; i.e. "Jan"
-s - seconds; i.e. "00" to "59"
-S - English ordinal suffix, textual, 2 characters; i.e. "th", "nd"
-t - number of days in the given month; i.e. "28" to "31"
-T - Timezone setting of this machine; i.e. "MDT"
-U - seconds since the epoch
-w - day of the week, numeric, i.e. "0" (Sunday) to "6" (Saturday)
-Y - year, 4 digits; i.e. "1999"
-y - year, 2 digits; i.e. "99"
-z - day of the year; i.e. "0" to "365"
-Z - timezone offset in seconds (i.e. "-43200" to "43200")
+
+//%%%%% FORMATAÇÃO %%%%%
+
+DIA --- ---
+d -> Dia do mês, 2 digitos com preenchimento de zero 01 até 31
+D -> Uma representação textual de um dia, três letras Mon até Sun
+j -> Dia do mês sem preenchimento de zero 1 até 31
+l -> ('L' minúsculo) A representação textual completa do dia da semana Sunday até Saturday
+N -> Representação numérica ISO-8601 do dia da semana (adicionado no PHP 5.1.0) 1 (para Segunda) até 7 (para Domingo)
+S -> Sufixo ordinal inglês para o dia do mês, 2 caracteres st, nd, rd ou th. Funciona bem com j
+w -> Representação numérica do dia da semana 0 (para domingo) até 6 (para sábado)
+z -> O dia do ano (começando do 0) 0 through 365
+
+
+SEMANA --- ---
+W -> Número do ano da semana ISO-8601, semanas começam na Segunda (adicionado no PHP 4.1.0) Exemplo: 42 (the 42nd week in the year)
+
+MÊS --- ---
+
+F -> Um representação completa de um mês, como January ou March January até December
+m -> Representação numérica de um mês, com leading zeros 01 a 12
+M -> Uma representação textual curta de um mês, três letras Jan a Dec
+n -> Representação numérica de um mês, sem leading zeros 1 a 12
+t -> Número de dias de um dado mês 28 through 31
+
+ANO --- ---
+
+L -> Se está em um ano bissexto 1 se está em ano bissexto, 0 caso contrário.
+o -> Número do ano ISO-8601. Este tem o mesmo valor como Y, exceto que se o número da semana ISO (W) pertence ao anterior ou próximo ano, o ano é usado ao invés. (adicionado no PHP 5.1.0) Exemplos: 1999 ou 2003
+Y -> Uma representação de ano completa, 4 dígitos Exemplos: 1999 ou 2003
+y -> Uma representação do ano com dois dígitos Exemplos: 99 ou 03
+
+TEMPO --- ---
+a -> Antes/Depois de meio-dia em minúsculo am or pm
+A -> Antes/Depois de meio-dia em maiúsculo AM or PM
+B -> Swatch Internet time 000 até 999
+g -> Formato 12-horas de uma hora sem preenchimento de zero 1 até 12
+G -> Formato 24-horas de uma hora sem preenchimento de zero 0 até 23
+h -> Formato 12-horas de uma hora com zero preenchendo à esquerda 01 até 12
+H -> Formato 24-horas de uma hora com zero preenchendo à esquerda 00 até 23
+i -> Minutos com zero preenchendo à esquerda 00 até 59
+s -> Segundos, com zero preenchendo à esquerda 00 até 59
+u -> Milisegundos (adicionado no PHP 5.2.2) Exemplo: 54321
+
+
+FUSO-HORÁRIO --- ---
+e -> Identificador de Timezone (adicionado no PHP 5.1.0) Exemplos: UTC, GMT, Atlantic/Azores
+I -> (capital i) Se a data está ou não no horário de verão 1 se horário de verão, 0 caso contrário.
+O -> Diferença para Greenwich time (GMT) em horas Exemplo: +0200
+P -> Diferença para Greenwich time (GMT) com dois pontos entre horas e minutos (adicionado no PHP 5.1.3) Exemplo: +02:00
+T -> Abreviação de Timezone Exemplos: EST, MDT ...
+Z -> Timezone offset in seconds. The offset for timezones west of UTC is always negative, and for those east of UTC is always positive. -43200 até 50400
+
+FULL DATE/TIME --- ---
+
+c -> ISO 8601 date (adicionado no PHP 5) 2004-02-12T15:19:21+00:00
+r -> » RFC 2822 formatted date Exemplo: Thu, 21 Dec 2000 16:01:07 +0200
+U -> Segundos desde a Época Unix (January 1 1970 00:00:00 GMT) Veja também time()
+
 */
 
+//%%%%% TIME FORMAT SETTINGS %%%%%
+if (!defined("_DATESTRING")) define("_DATESTRING","d/m/Y G:i:s");
+if (!defined("_MEDIUMDATESTRING")) define("_MEDIUMDATESTRING","Y/n/j G:i");
+if (!defined("_SHORTDATESTRING")) define("_SHORTDATESTRING","Y/n/j");
+if (!defined("_JSDATEPICKSTRING")) define("_JSDATEPICKSTRING","yy-mm-dd");
+if (!defined("_PHPDATEPICKSTRING")) define("_PHPDATEPICKSTRING","Y-m-d");
 
-//%%%%%		LANGUAGE SPECIFIC SETTINGS   %%%%%
-if (!defined('_CHARSET')) define('_CHARSET', 'utf-8');
-if (!defined('_LANGCODE')) define('_LANGCODE', 'pt');
+//%%%%% LANGUAGE SPECIFIC SETTINGS %%%%%
+if (!defined("_CHARSET")) define("_CHARSET", "utf-8");
+if (!defined("_LANGCODE")) define("_LANGCODE", "pt");
 // change 0 to 1 if this language is a multi-bytes language
-if (!defined('XOOPS_USE_MULTIBYTES')) define("XOOPS_USE_MULTIBYTES", "0");
+if (!defined("XOOPS_USE_MULTIBYTES")) define("XOOPS_USE_MULTIBYTES", "0");
 
-
-//%%%%%     REQUSTED DATA SETTINGS   %%%%%
-if (!defined('_REQUESTED_DATA_NAME')) define('_REQUESTED_DATA_NAME', 'requested_data_name');
-if (!defined('_REQUESTED_ACTION_NAME')) define('_REQUESTED_ACTION_NAME', 'requested_action_name');
-if (!defined('_REQUESTED_DATA_ID')) define('_REQUESTED_DATA_ID', 'requested_data_id');
-
+//%%%%% REQUESTED DATA SETTINGS %%%%%
+if (!defined("_REQUESTED_DATA_NAME")) define("_REQUESTED_DATA_NAME", "requested_data_name");
+if (!defined("_REQUESTED_ACTION_NAME")) define("_REQUESTED_ACTION_NAME", "requested_action_name");
+if (!defined("_REQUESTED_DATA_ID")) define("_REQUESTED_DATA_ID", "requested_data_id");
 ?>
