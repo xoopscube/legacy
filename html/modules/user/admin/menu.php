bea6467fcc44e7b6e864cc479a806fe70552ab80
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-05-25 13:43:34 +0000

    Fix Bug #2993387 Use of undefined constant _MI_USER_ADMENU_USER_DATA_DOWNLOAD
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@634 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/user/admin/menu.php
+++ html/modules/user/admin/menu.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined('XOOPS_ROOT_PATH')) exit();
+
 $adminmenu[10]['title'] = _MI_USER_MENU_CREATE_USER;
 $adminmenu[10]['link'] = "admin/index.php?action=UserEdit";
 $adminmenu[10]['keywords'] = _MI_USER_KEYWORD_CREATE_USER;
