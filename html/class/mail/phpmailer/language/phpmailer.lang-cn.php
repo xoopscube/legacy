d74cb58bf7efe4ca3b16c2e6126efba860969339
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-05-25 13:54:29 +0000

    Fix Bug #2993377 - syntax error in phpmailer.lang-cn.php with path disclosure
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@635 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/class/mail/phpmailer/language/phpmailer.lang-cn.php
+++ html/class/mail/phpmailer/language/phpmailer.lang-cn.php
@@ -11,9 +11,9 @@ $PHPMAILER_LANG["mailer_not_supported"] = '邮件功能不支持';
 $PHPMAILER_LANG["execute"] = '无法执行: ';
 $PHPMAILER_LANG["instantiate"] = '无法使用MAIL功能';
 $PHPMAILER_LANG["authenticate"] = 'SMTP 错误 : 无法通过验证.';
-$PHPMAILER_LANG["from_failed"] = 以下寄件地址错误 : ';
+$PHPMAILER_LANG["from_failed"] = '以下寄件地址错误 : ';
 $PHPMAILER_LANG["recipients_failed"] = 'SMTP 错误: 以下 ' .
-                                       '接收失败: ';
+									   '接收失败: ';
 $PHPMAILER_LANG["data_not_accepted"] = 'SMTP 错误 : 资料不可使用.';
 $PHPMAILER_LANG["connect_host"] = 'SMTP错误 : 无法连结SMTP伺服器';
 $PHPMAILER_LANG["file_access"] = '无法使用文件 : ';
