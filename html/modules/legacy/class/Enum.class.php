5aa14ebd7d783b438a1377530797745447bad215
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-05-26 12:43:26 +0000

    Add Image Type class to Enum.class.php
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@644 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/legacy/class/Enum.class.php
+++ html/modules/legacy/class/Enum.class.php
@@ -44,4 +44,26 @@ interface Lenum_GroupRank
 }
 
 
+class Lenum_ImageType
+{
+	const GIF = 1;
+	const JPG = 2;
+	const PNG = 3;
+
+	public static function getName(/*** Enum ***/ $ext)
+	{
+		switch($ext){
+		case self::GIF:
+			return 'gif';
+			break;
+		case self::JPG:
+			return 'jpg';
+			break;
+		case self::PNG:
+			return 'png';
+			break;
+		}
+	}
+}
+
 ?>
