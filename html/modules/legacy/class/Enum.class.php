9dd2283d34dcf479d88441b7a22f62b2efd0099a
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-07-05 13:13:03 +0000

    add getList() function to Lenum_GroupRank
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@688 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/legacy/class/Enum.class.php
+++ html/modules/legacy/class/Enum.class.php
@@ -34,13 +34,24 @@ interface Lenum_ProgressStatus
 /**
  * Lenum_GroupRank
 **/
-interface Lenum_GroupRank
+class Lenum_GroupRank
 {
 	const GUEST = 0;
 	const ASSOCIATE = 2;
 	const REGULAR = 5;
 	const STAFF = 7;
 	const OWNER = 9;
+
+	public static function getList()
+	{
+		return array(
+			self::GUEST => _GROUP_RANK_GUEST,
+			self::ASSOCIATE => _GROUP_RANK_ASSOCIATE,
+			self::REGULAR => _GROUP_RANK_REGULAR,
+			self::STAFF => _GROUP_RANK_STAFF,
+			self::OWNER => _GROUP_RANK_OWNER
+		);
+	}
 }
 
 
