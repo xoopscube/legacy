17585e2faed5d6b63fee01df5ff93355ec5f9919
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-07-06 05:20:28 +0000

    Updated: Portuguese language files
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@694 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_modules/html/modules/mydhtml/language/portuguese/modinfo.php
+++ extras/extra_modules/html/modules/mydhtml/language/portuguese/modinfo.php
@@ -3,9 +3,8 @@
  * @file
  * @package mydhtml
  * @version $Id$
- */
+*/
 
-define('_MI_MYDHTML_LANG_MYDHTML', "MYDHTML");
-define('_MI_MYDHTML_DESC_MYDHTML', "MYDHTML");
-
+define("_MI_MYDHTML_LANG_MYDHTML", "MyDHTML");
+define("_MI_MYDHTML_DESC_MYDHTML", "Editor MYDHTML");
 ?>
