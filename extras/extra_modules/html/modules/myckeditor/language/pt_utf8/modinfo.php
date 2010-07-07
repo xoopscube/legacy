3bdfbcf6267d5a5e88aa3e3577e822a07b9f311f
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-07-07 19:02:25 +0000

    Updated: Portuguese language files
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@697 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_modules/html/modules/myckeditor/language/pt_utf8/modinfo.php
+++ extras/extra_modules/html/modules/myckeditor/language/pt_utf8/modinfo.php
@@ -6,5 +6,5 @@
 */
 
 define("_MI_MYCKEDITOR_LANG_MYCKEDITOR", "MyCKEditor");
-define("_MI_MYCKEDITOR_DESC_MYCKEDITOR", "Versão modularizada do <a href='http://ckeditor.com/'>MyCKEditor</a>, um editor avançado de textos que provê as funções e características mais comuns encontradas em aplicativos como o Microsoft Word e OpenOffice. Muito elogiada mundialmente, é grátis e remendada e usada por empresas como IBM, Adobe, Oracle e várias outras, já que conta com licença aberta para uso pessoal ou comercial, sem necessidade de autorização.");
+define("_MI_MYCKEDITOR_DESC_MYCKEDITOR", "Versão modularizada do CKEditor ( http://ckeditor.com/ ), um editor avançado de textos que provê as funções e características mais comuns encontradas em aplicativos como o Microsoft Word e OpenOffice. Muito elogiada mundialmente, é grátis e remendada e usada por empresas como IBM, Adobe, Oracle e várias outras, já que conta com licença aberta para uso pessoal ou comercial, sem necessidade de autorização.");
 ?>
--- extras/extra_modules/html/modules/mydhtml/language/portuguese/modinfo.php
+++ extras/extra_modules/html/modules/mydhtml/language/portuguese/modinfo.php
@@ -6,5 +6,5 @@
 */
 
 define("_MI_MYDHTML_LANG_MYDHTML", "MyDHTML");
-define("_MI_MYDHTML_DESC_MYDHTML", "Editor MYDHTML");
+define("_MI_MYDHTML_DESC_MYDHTML", "Editor MyDHTML");
 ?>
