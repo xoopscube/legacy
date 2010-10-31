6f2dea55881a5e7a825fe1321a73b89245034e5b
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2010-10-31 11:57:51 +0000

    Revert deleted language definitions for lecat
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/branches/r2_2_00-branch@824 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_modules/xoops_trust_path/modules/lecat/language/ja_utf8/modinfo.php
+++ extras/extra_modules/xoops_trust_path/modules/lecat/language/ja_utf8/modinfo.php
@@ -62,7 +62,5 @@ define('_MI_LECAT_LANG_CSS_FILE', 'CSS ファイルのURL');
 define('_MI_LECAT_DESC_CSS_FILE', 'カテゴリモジュール用のCSSファイルの場所を設定します（'.XOOPS_URL.'以降を入力）');
 define('_MI_LECAT_LANG_MAXDEPTH', '階層制限');
 define('_MI_LECAT_DESC_MAXDEPTH', 'カテゴリ階層の制限。0にすると無制限');
-define('_MI_LECAT_TPL_DEFAULT_SET', '初期テンプレートセット');
-define('_MI_LECAT_TPL_ACTOR_EDIT', '権限の編集');
 
 ?>
