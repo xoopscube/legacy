b9fe187df5dfd31f9b929b35ed90798d05704260
bindi <bindi@704cf05f-ae62-4b0e-a484-234ee0250e75>
2011-10-22 08:51:58 +0000

    merge with the r2_2_00-branch to trunk. delete launguages in extras/extra_languages/
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1007 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/pt_utf8/extras/system/language/pt_utf8/admin/avatars.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("_MD_AVATARMAN","Administração dos Avatares");
-define("_MD_SYSAVATARS","Avatares do sistema");
-define("_MD_CSTAVATARS","Avatares enviados");
-define("_MD_ADDAVT","Acrescentar avatar");
-define("_MD_USERS","associados usando este avatar:");
-define("_MD_RUDELIMG","Tem certeza de que deseja remover este avatar?");
-define("_MD_FAILDEL","Não foi possível remover o avatar %s do banco de dados.");
-?>
