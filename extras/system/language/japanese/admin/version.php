38ac8a99415bcc1253a48d8735fbe6a58be40e67
bindi <bindi@704cf05f-ae62-4b0e-a484-234ee0250e75>
2011-10-22 09:09:12 +0000

    merge with the r2_2_00-branch to trunk. delete system in extras/system/
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1009 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/system/language/japanese/admin/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-//%%%%%%	Admin Module Name  Version 	%%%%%
-define("_AM_DBUPDATED",_MD_AM_DBUPDATED);
-
-
-?>
--- extras/system/language/portuguese/admin/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-// $Id$
-// License http://creativecommons.org/licenses/by/2.5/br/
-define("_AM_DBUPDATED","Informações atualizadas corretamente.");
-?>
