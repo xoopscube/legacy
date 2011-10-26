844e383df902f2779d45daaa1e192a2ae3d7de0d
bindi <bindi@704cf05f-ae62-4b0e-a484-234ee0250e75>
2011-10-26 21:59:22 +0000

    fix check variable
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1015 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/install/templates/install_start.tpl.php
+++ html/install/templates/install_start.tpl.php
@@ -1,4 +1,4 @@
-<?php if($message){ ?>
+<?php if(isset($message)){ ?>
 <p align="center"><?php $this->e('message')?></p>
 <?php }else{ ?>
 <div class="confirmInfo"><?php $this->e('welcome') ?></div>
