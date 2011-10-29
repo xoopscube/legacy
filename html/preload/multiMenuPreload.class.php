eb68ae371788a9b5b0b1aad68ba0bf9f737c96af
dashboard <dashboard@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-10-29 11:01:00 +0000

    Check is_active.
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@23 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- html/preload/multiMenuPreload.class.php
+++ html/preload/multiMenuPreload.class.php
@@ -6,12 +6,19 @@
  * GPL ver3.0 All right reserved.
  */
 if (!defined('XOOPS_ROOT_PATH')) exit();
+
 include_once XOOPS_ROOT_PATH . '/modules/multiMenu/class/getMultiMenu.class.php';
+
 class multiMenuPreload extends XCube_ActionFilter{
 	function preBlockFilter(){
 		$this->mRoot->mDelegateManager->add('Legacy_RenderSystem.SetupXoopsTpl', array(&$this, 'menuSmartyAssign'));
 	}
 	function menuSmartyAssign(&$xoopsTpl) {
+    	$module_handler = & xoops_gethandler( 'module' );
+		$module =& $module_handler->getByDirname("multiMenu");
+		if ( !is_object( $module ) || !$module->getVar( 'isactive' ) ) {
+			return NULL;
+		}
 		$gmm = new getMultiMenu();
 		$options=array("40");
 		$block = $gmm->getblock( $options, "multimenu0" . $gmm->theme_menu() ); 
