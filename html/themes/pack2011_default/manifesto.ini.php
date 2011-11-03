790c154721e596a95d7f813d722260464eea9837
argon <argon@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-03 18:54:55 +0000

    bit modify.
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@60 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- html/themes/pack2011_default/manifesto.ini.php
+++ html/themes/pack2011_default/manifesto.ini.php
@@ -3,16 +3,16 @@
 
 [Manifesto]
 Name="pack2011_default"
-Depends=Legacy_RenderSystem,legacy
+Depends="Legacy_RenderSystem,legacy"
 Url="http://sourceforge.jp/projects/xoopscube22x/"
 Version="0.5"
 
 [Theme]
 RenderSystem="Legacy_RenderSystem"
 Format="XOOPS2 Legacy Style"
-Author="XOOPS Cube 2.2 Distribution Pack 2011 team"
+Author=XOOPS Cube 2.2 Distribution Pack 2011 Team"
 ScreenShot="screenshot.png"
-Description="Layout with 2 Columns. Responsive Web Design. Using MediaQueries Multi-Device Template. thanks unnythingz. Main committer Yoshi Sakai aka bluemooninc and argon."
+Description="Layout with 2 Columns. Responsive Web Design. Using MediaQueries Multi-Device Template. Thanks funnythingz. This Theme main committer is Yoshi Sakai aka bluemooninc and argon."
 
 Licence="GPL"
 
