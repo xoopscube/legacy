c1106534c651664cb066beab9773828c8c3acbf4
argon <argon@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-03 18:38:18 +0000

    add manifesto.ini.php and screenshot.png.
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@59 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

---
+++ html/themes/pack2011_default/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="pack2011_default"
+Depends=Legacy_RenderSystem,legacy
+Url="http://sourceforge.jp/projects/xoopscube22x/"
+Version="0.5"
+
+[Theme]
+RenderSystem="Legacy_RenderSystem"
+Format="XOOPS2 Legacy Style"
+Author="XOOPS Cube 2.2 Distribution Pack 2011 team"
+ScreenShot="screenshot.png"
+Description="Layout with 2 Columns. Responsive Web Design. Using MediaQueries Multi-Device Template. thanks unnythingz. Main committer Yoshi Sakai aka bluemooninc and argon."
+
+Licence="GPL"
+
+*/
+?>
