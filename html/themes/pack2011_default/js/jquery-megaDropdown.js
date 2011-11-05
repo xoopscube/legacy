b55215a502220975ccb3a1bf51c294547836443b
dashboard <dashboard@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-05 01:28:21 +0000

    mega drop js
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@80 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- html/themes/pack2011_default/js/jquery-megaDropdown.js
+++ html/themes/pack2011_default/js/jquery-megaDropdown.js
@@ -5,30 +5,64 @@
 * 
 * jquery-megaDropdown.js
 */
-(function($){
-	$.fn.megaDropdown = function(config){
-		var defaults = {
-			effectSpeed: 250
-		}
-		var options = $.extend(defaults, config);
-		return this.each(function(i){
-			var self = $(this);
-			var contents = $('.gnaviChildCol', self);
-			var bgClass = 'megaDropdownOver';
+$(document).ready(function() {
+	
+
+	function megaHoverOver(){
+		$(this).find(".sub").stop().fadeTo('fast', 1).show();
 			
-			contents.hide();
-			self.bind('mouseenter mouseleave', i, function(e){
-				if( $(window).width() > 581 ){
-					if( e.type === 'mouseenter' ){
-						contents.stop(true, true).fadeIn(options.effectSpeed);
-						self.addClass(bgClass);
-					}
-					else if( e.type === 'mouseleave' ){
-						contents.hide();
-						self.removeClass(bgClass);
-					}
+		//Calculate width of all ul's
+		(function($) { 
+			jQuery.fn.calcSubWidth = function() {
+				rowWidth = 0;
+				//Calculate row
+				$(this).find("ul").each(function() {					
+					rowWidth += $(this).width(); 
+				});	
+			};
+		})(jQuery); 
+		
+		if ( $(this).find(".row").length > 0 ) { //If row exists...
+			var biggestRow = 0;	
+			//Calculate each row
+			$(this).find(".row").each(function() {							   
+				$(this).calcSubWidth();
+				//Find biggest row
+				if(rowWidth > biggestRow) {
+					biggestRow = rowWidth;
 				}
 			});
-		});
+			//Set width
+			$(this).find(".sub").css({'width' :biggestRow});
+			$(this).find(".row:last").css({'margin':'0'});
+			
+		} else { //If row does not exist...
+			
+			$(this).calcSubWidth();
+			//Set Width
+			$(this).find(".sub").css({'width' : rowWidth});
+			
+		}
+	}
+	
+	function megaHoverOut(){ 
+	  $(this).find(".sub").stop().fadeTo('fast', 0, function() {
+		  $(this).hide(); 
+	  });
+	}
+
+
+	var config = {    
+		 sensitivity: 2, // number = sensitivity threshold (must be 1 or higher)    
+		 interval: 100, // number = milliseconds for onMouseOver polling interval    
+		 over: megaHoverOver, // function = onMouseOver callback (REQUIRED)    
+		 timeout: 500, // number = milliseconds delay before onMouseOut    
+		 out: megaHoverOut // function = onMouseOut callback (REQUIRED)    
 	};
-})(jQuery);
+
+	$("ul#topnav li .sub").css({'opacity':'0'});
+	$("ul#topnav li").hoverIntent(config);
+
+
+
+});
