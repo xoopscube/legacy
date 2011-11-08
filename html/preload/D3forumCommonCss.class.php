86f429a05a86b6a618ea33c4d98f1145c0911d01
naao <naao@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-08 07:48:59 +0000

    CommonCss (thx photosite) Preload modified by naao for d3forum comment integrated pagenavi style
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@119 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

---
+++ html/preload/D3forumCommonCss.class.php
@@ -0,0 +1,28 @@
+<?php
+//================================================================
+// CommonCss Preload
+// by  photosite, http://www.photositelinks.com/
+//================================================================
+// modified by naao for d3forum comment integrated pagenavi style
+
+if (!defined('XOOPS_ROOT_PATH')) exit();
+
+class D3forumCommonCss extends XCube_ActionFilter
+{
+	function preBlockFilter()
+	{
+		$this->mRoot->mDelegateManager->add('XoopsTpl.New', array(&$this, 'D3forumCommonCssfile'));
+
+	}
+	function D3forumCommonCssfile(&$xoopsTpl)
+	{
+		$filename = XOOPS_URL.'/common/css/d3forum_common.css';
+		$filepath = XOOPS_ROOT_PATH.'/common/css/d3forum_common.css';
+		if( file_exists( $filepath ) ) {
+			$xoops_module_header = '<link rel="stylesheet" type="text/css" media="screen" href="'.$filename.'" />';
+			$xoopsTpl->assign('xoops_module_header', $xoops_module_header . "\n" . $xoopsTpl->get_template_vars('xoops_module_header'));
+		}
+	}
+}
+
+?>
