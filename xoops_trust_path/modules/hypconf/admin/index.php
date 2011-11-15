cf8f9f3202c36dcf9405f0c9b1330ae85391ba01
nao-pon <nao-pon@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-15 00:50:43 +0000

    Added writable check.
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@204 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- xoops_trust_path/modules/hypconf/admin/index.php
+++ xoops_trust_path/modules/hypconf/admin/index.php
@@ -1,34 +1,52 @@
 <?php
 
-$op = '';
-
-if (isset($_POST) && isset($_POST['op'])) {
-	$op = $_POST['op'];
+if (! $data = @ file_get_contents(XOOPS_TRUST_PATH . HYP_COMMON_PRELOAD_CONF)) {
+	$data = '';
 }
-
-if ($op === 'save') {
-	var_dump($_POST);
-
-} else {
-
-	if (! $data = @ file_get_contents(XOOPS_TRUST_PATH . HYP_COMMON_PRELOAD_CONF)) {
-		$data = '';
-	}
-	$data = htmlspecialchars($data);
-	$content = <<<EOD
+$data = htmlspecialchars($data);
+$content = <<<EOD
 <div style="max-width:700px;">
  <pre style="width:100%;height:25em;overflow:auto;padding:1em;border:1px solid gray">$data</pre>
 </div>
 EOD;
 
-	xoops_cp_header();
+$check_dir = array(
+	XOOPS_TRUST_PATH.'/cache',
+	XOOPS_TRUST_PATH.'/class/hyp_common/favicon/cache',
+	XOOPS_TRUST_PATH.'/uploads/hyp_common',
+	XOOPS_TRUST_PATH.'/uploads/hyp_common/kakasi',
+);
 
-	include dirname(__FILE__).'/mymenu.php' ;
+$dir_res = array();
 
-	echo '<h3>'.hypconf_constant($constpref . '_DESC').' - '.hypconf_constant($constpref . '_ADMENU_CONTENTSADMIN').'</h3>' ;
-	echo '<h4>trust'.HYP_COMMON_PRELOAD_CONF.'</h4>' ;
-	echo $content;
-
-	xoops_cp_footer();
+foreach($check_dir as $dir){
+	$dir = rtrim($dir, '/');
+	if (is_writable($dir)) {
+		$dir .= ' (<span style="color:green;font-weight:bold;">OK</span>)';
+	} else {
+		$dir .= ' (<span style="color:red;font-weight:bold;">NG</span>)';
+	}
+	$dir_res[] = $dir;
 }
+
+$dir_res = '<ul><li>'.join('</li><li>', $dir_res).'</li></ul>';
+
+// output
+xoops_cp_header();
+
+include dirname(__FILE__).'/mymenu.php' ;
+
+echo '<h3>'.hypconf_constant($constpref . '_DESC').' - '.hypconf_constant($constpref . '_ADMENU_CONTENTSADMIN').'</h3>' ;
+echo '<h4>trust'.HYP_COMMON_PRELOAD_CONF.'</h4>' ;
+echo <<<EOD
+
+$content
+
+<h3>Writable check results</h3>
+$dir_res
+
+EOD;
+
+xoops_cp_footer();
+
 exit();
--- xoops_trust_path/modules/xpwiki/admin/index.php
+++ xoops_trust_path/modules/xpwiki/admin/index.php
@@ -7,9 +7,44 @@ if (!$xw->func->get_pgid_by_name($xw->root->defaultpage)) {
 	$xw->func->send_location('', '', $xw->cont['HOME_URL'] . '?cmd=dbsync');
 }
 
+$check_dir = array(
+	$xw->cont['DATA_DIR']        ,
+	$xw->cont['DIFF_DIR']        ,
+	$xw->cont['BACKUP_DIR']      ,
+	$xw->cont['CACHE_DIR']       ,
+	$xw->cont['UPLOAD_DIR']      ,
+	$xw->cont['UPLOAD_DIR'].'s' ,
+	//$xw->cont['COUNTER_DIR']     ,
+	$xw->cont['TRACKBACK_DIR']   ,
+	$xw->cont['PLUGIN_DIR']      ,
+	$xw->cont['RENDER_CACHE_DIR']
+);
+
+$dir_res = array();
+
+foreach($check_dir as $dir){
+	$dir = rtrim($dir, '/');
+	if (is_writable($dir)) {
+		$dir .= ' (<span style="color:green;font-weight:bold;">OK</span>)';
+	} else {
+		$dir .= ' (<span style="color:red;font-weight:bold;">NG</span>)';
+	}
+	$dir_res[] = $dir;
+}
+
+$dir_res = '<ul><li>'.join('</li><li>', $dir_res).'</li></ul>';
+
+// output
 xoops_cp_header() ;
 
 include dirname(__FILE__).'/mymenu.php' ;
+
+echo <<<EOD
+
+<h3>Writable check results</h3>
+$dir_res
+
+EOD;
 
 xoops_cp_footer() ;
 
