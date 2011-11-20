e7a114e93e3b819a724f1610e17b508749e9a496
hamaco <hamaco@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-11-20 07:34:40 +0000

    アクセス権チェックに成功してるのにReloadボタンが表示されるバグ修正
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@243 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- html/install/custom/custom.inc.php
+++ html/install/custom/custom.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-	$wizardSeq->insertAfter('modcheck', 'modcheckext', _INSTALL_L81,'start',_INSTALL_L80,true);
-	$wizardSeq->insertAfter('modcheck_trust', 'modcheck_trustext', 'trust is_writeable Check','modcheck_trust',_INSTALL_L166,true);
+	$wizardSeq->insertAfter('modcheck', 'modcheckext', _INSTALL_L81, 'start', _INSTALL_L80, false);
+	$wizardSeq->insertAfter('modcheck_trust', 'modcheck_trustext', 'trust is_writeable Check','modcheck_trust',_INSTALL_L166, false);
 	$wizardSeq->insertAfter('siteInit', 'insertData_theme', _INSTALL_L117);
 	$wizardSeq->add('insertData_theme',  _INSTALL_L116, 'finish',     _INSTALL_L117);
 
