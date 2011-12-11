9e47098b9c184842294147f4b62822a5d5cda7d6
domifara <domifara@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-12-11 16:17:33 +0000

    [cubeUtils]langselect change for W3C
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@357 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- html/modules/cubeUtils/blocks/cubeUtils_langsel.php
+++ html/modules/cubeUtils/blocks/cubeUtils_langsel.php
@@ -18,21 +18,21 @@ function b_cubeUtils_langsel_show($options) {
     if (empty($_SERVER['QUERY_STRING'])) {
         $pagenquery = $_SERVER['PHP_SELF'].'?'.CUBE_UTILS_ML_PARAM_NAME.'=';
     } elseif (isset($_SERVER['QUERY_STRING'])) {
-        
+
         $query = explode("&",$_SERVER['QUERY_STRING']);
         $langquery = $_SERVER['QUERY_STRING'];
-        
+
         // If the last parameter of the QUERY_STRING is sel_lang, delete it so we don't have repeating sel_lang=...
         If (strpos($query[count($query) - 1], CUBE_UTILS_ML_PARAM_NAME.'=')  === 0 ) {
             $langquery = str_replace('&' . $query[count($query) - 1], '', $langquery);
         }
-        
+
         $pagenquery = $_SERVER['PHP_SELF'].'?'.$langquery.'&'.CUBE_UTILS_ML_PARAM_NAME.'=';
         $pagenquery = str_replace('?&','?',$pagenquery);
     }
 
     //show a drop down list to select language
-    
+
     $block['content'] = "<script type='text/javascript'>
 <!--
 function SelLang_jumpMenu(targ,selObj,restore){
@@ -41,18 +41,20 @@ if (restore) selObj.selectedIndex=0;
 }
 -->
 </script>";
-    $block['content'] .= '<div align="center"><select name="'.CUBE_UTILS_ML_PARAM_NAME.'" onChange="SelLang_jumpMenu(\'parent\',this,0)">';
+    $block['content'] .= '<div align="center"><select name="'.CUBE_UTILS_ML_PARAM_NAME.'" onchange="SelLang_jumpMenu(\'parent\',this,0)">';
     $languages = XoopsLists::getLangList();
     $langnames = explode(',',CUBE_UTILS_ML_LANGDESCS);
     $langs = explode(',',CUBE_UTILS_ML_LANGS);
     for ($i=0; $i < count($langs); $i++) {
         $block['content'] .= '<option value="'.$langs[$i].'"';
-        if ($langs[$i] == _LANGCODE) $block['content'] .= " selected";
+        if ($langs[$i] == _LANGCODE){
+        	$block['content'] .= " selected=\"selected\"";
+        }
         $block['content'] .= '>'.$langnames[$i].'</option>';
     }
-    
+
     $block['content'] .= '</select></div>';
-    
+
     return $block;
 }
 ?>
