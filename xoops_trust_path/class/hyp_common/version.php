f88d1b5e672839c0f478861ff3d8d376ef738382
nao-pon <nao-pon@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2011-12-14 00:17:59 +0000

    [hyp_common, xpwiki] Version numbar update.
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@379 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- xoops_trust_path/class/hyp_common/version.php
+++ xoops_trust_path/class/hyp_common/version.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/05/30 by nao-pon http://hypweb.net/
- * $Id: version.php,v 1.131 2011/12/13 08:17:59 nao-pon Exp $
+ * $Id: version.php,v 1.132 2011/12/14 00:00:24 nao-pon Exp $
  */
 
-$version = '20111213';
+$version = '20111214';
--- xoops_trust_path/modules/xpwiki/version.php
+++ xoops_trust_path/modules/xpwiki/version.php
@@ -1,2 +1,2 @@
-<?php $xpwiki_version = '5.01.5';
+<?php $xpwiki_version = '5.01.6';
 if (! defined('XPWIKI_VERSION')) define('XPWIKI_VERSION', $xpwiki_version);
