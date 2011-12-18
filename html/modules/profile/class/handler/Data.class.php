e2172cd2f79655471d5466ab2dbf1a267d528641
kilica <kilica@704cf05f-ae62-4b0e-a484-234ee0250e75>
2011-12-18 08:49:02 +0000

    Fix Call-time pass-by-reference notice in Profile
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1083 704cf05f-ae62-4b0e-a484-234ee0250e75

--- html/modules/profile/class/handler/Data.class.php
+++ html/modules/profile/class/handler/Data.class.php
@@ -81,7 +81,7 @@ class Profile_DataHandler extends XoopsObjectGenericHandler
 		if(count($obj->mDef)===0){
 			return true;
 		}
-		parent::insert(&$obj, $force = false);
+		parent::insert($obj, $force = false);
 	}
 
 }
