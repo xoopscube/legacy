f95f879cad82dd27547a61a926dd608845f6e9cf
xoopserver <xoopserver@704cf05f-ae62-4b0e-a484-234ee0250e75>
2011-12-26 02:21:45 +0000

    extra modules myckeditor language french
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1114 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_modules/html/modules/myckeditor/language/english/modinfo.php
+++ extras/extra_modules/html/modules/myckeditor/language/english/modinfo.php
@@ -6,5 +6,5 @@
  */
 
 define('_MI_MYCKEDITOR_LANG_MYCKEDITOR', "CKEditor");
-define('_MI_MYCKEDITOR_DESC_MYCKEDITOR', "http://ckeditor.com/");
+define('_MI_MYCKEDITOR_DESC_MYCKEDITOR', "Wysiwig Editor for textareas of XOOPS Cube Legacy.");
 ?>
