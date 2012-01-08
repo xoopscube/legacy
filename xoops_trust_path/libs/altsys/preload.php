6604db61ed3567446950ae3a10109bd41a62c990
domifara <domifara@4dba175d-8fb6-45ee-ba88-c0098d4d2fdc>
2012-01-08 02:08:36 +0000

    [altsys]commit mis revers XCL2.2.1beat1 xoops_trust_path/libs/altsys/preload.php
    git-svn-id: https://svn.sourceforge.jp/svnroot/xoopscube22x/trunk@437 4dba175d-8fb6-45ee-ba88-c0098d4d2fdc

--- xoops_trust_path/libs/altsys/preload.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('XOOPS_ROOT_PATH')) exit();
-include_once dirname(__FILE__).'/include/altsys_functions.php' ;
-// language file (modinfo.php)
-altsys_include_language_file( 'modinfo' ) ;
-?>
