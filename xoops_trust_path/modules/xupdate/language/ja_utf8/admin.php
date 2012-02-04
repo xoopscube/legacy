d57d80f4d4fe2e3bfc620cc8a7d08ba412ef9857
domifara <domifara@68c2734e-149c-420d-9c06-e9d97fbf3a51>
2012-02-04 22:41:00 +0000

    [xupdate]ストアー一括インストール #304 http://www.xoopscube.net/redmine/issues/304
    git-svn-id: https://svn.sourceforge.jp/svnroot/xupdate/trunk@30 68c2734e-149c-420d-9c06-e9d97fbf3a51

--- xoops_trust_path/modules/xupdate/language/ja_utf8/admin.php
+++ xoops_trust_path/modules/xupdate/language/ja_utf8/admin.php
@@ -6,8 +6,17 @@
 **/
 
 define('_AD_XUPDATE_LANG_WRITABLE_RESULT', "フォルダ権限チェック結果");
+define('_AD_XUPDATE_LANG_FTP_PASS_RESULT', "FTP設定チェック結果");
 
 define('_AD_XUPDATE_LANG_MODULES', "モジュール一覧");
+
+define('_AD_XUPDATE_LANG_TYPE', "タイプ");
+
+define('_AD_XUPDATE_LANG_MESSAGE_BATCH_INSTALL', "一括インストール");
+define('_AD_XUPDATE_LANG_MESSAGE_WAITING', "待機中");
+define('_AD_XUPDATE_LANG_MESSAGE_SUCCESS', "完了");
+define('_AD_XUPDATE_LANG_MESSAGE_GETTING_FILES', "配置");
+define('_AD_XUPDATE_LANG_MESSAGE_PROCESSING', "中");
 
 
 ?>
