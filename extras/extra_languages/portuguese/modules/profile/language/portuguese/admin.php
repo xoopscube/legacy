1184dc1774e1c6d06403103e610542d484339863
mikhail <mikhail@704cf05f-ae62-4b0e-a484-234ee0250e75>
2012-05-03 17:01:06 +0000

    Update Portuguese language files.
    git-svn-id: https://xoopscube.svn.sourceforge.net/svnroot/xoopscube/Package_Legacy/trunk@1150 704cf05f-ae62-4b0e-a484-234ee0250e75

--- extras/extra_languages/portuguese/modules/profile/language/portuguese/admin.php
+++ extras/extra_languages/portuguese/modules/profile/language/portuguese/admin.php
@@ -18,4 +18,16 @@ define("_AD_PROFILE_LANG_DEFINITIONS_DELETE","Remover campo");
 define("_AD_PROFILE_LANG_DEFINITIONS_EDIT","Editar campo");
 define("_AD_PROFILE_LANG_DEFINITIONS_VIEW","Mostrar campo");
 define("_AD_PROFILE_TIPS_DATA_DOWNLOAD", "Os dados exportados em CSV são ordenados pelo número do cadastro do associado (<i>user_id</i>).");
+//
+define("_AD_PROFILE_DATA_UPLOAD_CONF", "Confirme a inclusão do conteúdo.");
+define("_AD_PROFILE_DATA_UPLOAD_DO", "Cadastre-se");
+define("_AD_PROFILE_DATA_UPLOAD_DONE", "Os dados de perfil foram atualizados de acordo com dados do arquivo CSV enviado.");
+define("_AD_PROFILE_DATA_UPLOAD_SELECT_CSVFILE", "Por favor, selecione o arquivo de CSV compatível.");
+define("_AD_PROFILE_TIPS1_DATA_UPLOAD", "É possível incluir vários cadastros de uma só vez a partir de um arquivo CSV.");
+define("_AD_PROFILE_TIPS2_DATA_UPLOAD", "Utilize apenas arquivos que seguem o padrão daqueles gerados pelo recurso <q><a href='./?action=UserDataDownload' style='color:#941d55;font-weight:bold;'>Exportar cadastros no formato CSV</a></q>. Não crie ou remova nenhuma coluna.");
+define("_AD_PROFILE_TIPS3_DATA_UPLOAD", "Please describe only the user who wants to update and wants to register information newly in CSV file.");
+define("_AD_PROFILE_TIPS4_DATA_UPLOAD", "When the row of leftmost UID is emptied (or 0), it would not work.");
+define("_AD_PROFILE_TIPS5_DATA_UPLOAD", "The profile information is updated when there is a value of the row of leftmost(UID).");
+define("_AD_USER_DATA_UPLOAD_BACK", "Selecione o arquivo CSV de novo.");
+define("_AD_USER_DATA_UPLOAD_CHECK_USER_CSVFILE", "Por favor, confirme a inclusão dos dados.");
 ?>
