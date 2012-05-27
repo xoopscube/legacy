05dec5f9334b866125796f2fea5bea51134608f5
Naoki Okino <naao@mbg.nifty.com>
2012-05-27 16:59:24 +0900

    fixed degraded profile module's "ja_utf8" language files after added CSV upload function profileモジュールのCSVアップロードで言語ファイルの配置ミスでデグレードされていたのを戻した

--- html/modules/profile/admin/actions/DataUploadDoAction.class.php
+++ html/modules/profile/admin/actions/DataUploadDoAction.class.php
@@ -17,41 +17,112 @@ class Profile_Admin_DataUploadDoAction extends Profile_Admin_DataUploadAction
 			return $this->getDefaultView($controller, $xoopsUser);
 		}
 		/// csv file check
-		if (isset($_SESSION['profile_csv_upload_data']) &&
-			count($_SESSION['profile_csv_upload_data'])){
+		if (isset($_SESSION['import_csv_upload_data']) &&
+			count($_SESSION['import_csv_upload_data'])){
 			return PROFILE_FRAME_VIEW_SUCCESS;
 		}
 		return $this->getDefaultView();
 	}
-	
-	
-	function executeViewSuccess(&$controller,&$render)
+	/*
+	 *  Insert & Update for groups_users_link table
+	 */
+	function insertGroups(&$handler,$uid,$groupIds)
 	{
-		$csv_data = $_SESSION['profile_csv_upload_data'];
-		$profile_handler =& xoops_getmodulehandler('data');
-		$defHandler =& xoops_getmodulehandler('definitions');
-		$defArr =& $defHandler->getDefinitions(false);
-		$profile_key[] = "uid";
-		foreach (array_keys($defArr) as $key){
-			$profile_key[] = $key;
-		}		
-		foreach ($csv_data as $data){
-			if ($data['is_new'] || $data['update']){
-				if ($data['update']){
-					$prof =& $profile_handler->get($data['value'][0]['var']);
-				} else {
-					$prof =& $profile_handler->create();
-				}
-				foreach ($profile_key as $i=>$key){
-					$value = $data['value'][$i]['var'];
-					$prof->setVar($key, $value);
-					echo $value;
-				}
-				$profile_handler->insert($prof);
+		$flag = true;
+		// delete before insert
+		if ($uid){
+			$force = true;
+			$oldLinkArr = $handler->getObjects(new Criteria('uid', $uid), $force);
+			$oldGroupidArr = array();
+			foreach (array_keys($oldLinkArr) as $key) {
+				$handler->delete($oldLinkArr[$key], $force);
 			}
 		}
-		unset($_SESSION['profile_csv_upload_data']);
+		// insert gid
+		foreach ($groupIds as $gid) {
+			if ($gid){
+				$link = $handler->create();
+				$link->set('groupid', $gid);
+				$link->set('uid', $uid);
+				$flag &= $handler->insert($link, true);
+			}
+		}
+		return $flag;
+	}
+	function executeViewSuccess(&$controller,&$render)
+	{
+		$csv_data = $_SESSION['import_csv_upload_data'];
 		
-		$controller->executeRedirect("index.php", 1, _AD_PROFILE_DATA_UPLOAD_DONE);
+		// Get user table keys
+		$userHandler = xoops_getmodulehandler('users', 'user');
+		$user_tmp = $userHandler->create();
+		$userKey = array_keys($user_tmp->gets());
+		
+		// Get group table keys
+		$groupHandler = xoops_getmodulehandler ('groups_users_link', 'user');
+		$groupKey = array("groupid");		
+		
+		$profile_handler = xoops_getmodulehandler('data');
+		$defHandler = xoops_getmodulehandler('definitions');
+		$defArr = $defHandler->getDefinitions(false);
+		// Set key to restore
+		$import_key = array_merge($userKey,$groupKey);
+		foreach (array_keys($defArr) as $key){
+			$import_key[] = $key;
+		}
+		foreach ($csv_data as $data){
+			$uid = NULL;
+			if ($data['is_new'] || $data['userUpdate'] || $data['groupUpdate'] || $data['profUpdate']
+			 || $data['userCreate'] || $data['groupCreate'] || $data['profCreate'] ){
+				adump($data);
+				$userUpdate = $groupUpdate = $profUpdate = False;
+				// get uid
+				$uid = $data['value'][0]['var'];
+				if ($data['userUpdate']){
+					$userObj = $userHandler->get($uid);
+					$userUpdate = TRUE;
+				} elseif ($data['userCreate']) {
+					$userObj = $userHandler->create();
+					$userUpdate = TRUE;
+				}
+				if ($data['profCreate']){
+					$profObj = $profile_handler->create();
+					$profUpdate = TRUE;
+				}elseif ($data['profUpdate']){
+					$profObj = $profile_handler->get($uid);
+					$profUpdate = TRUE;
+				}
+				foreach ($import_key as $i=>$key){
+					$value = $data['value'][$i]['var'];
+					$field = $data['value'][$i]['field'];
+					if(!isset($value)) continue;
+					if (in_array($field,$userKey)){
+						if($key=="pass" && !(ctype_xdigit($value) && strlen($value)==32) ){
+							$value = md5($value);
+						}
+						if($userUpdate){
+							$userObj->setVar($key, $value);
+						}	
+					} elseif (in_array($field,$groupKey)){
+						$groupIds = explode("|",$value);
+						$groupUpdate = TRUE;	
+					} else{
+						if($profUpdate){
+							if($value) $profObj->setVar($key, $value);
+						}	
+					}
+				}
+				if ($userUpdate) $userHandler->insert($userObj);
+				if (is_null($uid)){
+					$uid = $userObj->getVar('uid');
+				} 
+				$profObj->setVar('uid', $uid);
+				if ($groupUpdate) $this->insertGroups($groupHandler,$uid,$groupIds);
+				if ($profUpdate) $profile_handler->insert($profObj);
+			}
+		}
+		unset($_SESSION['import_csv_upload_data']);
+		echo "User,Group,Profile Insert and Update Done.";
+		//$controller->executeRedirect("./index.php?action=DefinitionsList", 1, _AD_PROFILE_DATA_UPLOAD_DONE);
 	}
 }
