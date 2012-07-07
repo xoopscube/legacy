52320139fe3e11285c3326d2d892ad93b02721a0
nao-pon <hypweb@gmail.com>
2012-07-08 08:50:52 +0900

    reduce "_downloadFile()"

--- xoops_trust_path/modules/xupdate/class/Func.class.php
+++ xoops_trust_path/modules/xupdate/class/Func.class.php
@@ -44,128 +44,18 @@ class Xupdate_Func {
 	 **/
 	public function _downloadFile( $target_key, $downloadUrl, $tempFilename, &$downloadedFilePath, $cacheTTL = 0 )
 	{
-
-		$downloadDirPath = $this->Xupdate->params['temp_path'];
-		$realDirPath = realpath($downloadDirPath);
-
-		// make exploring directory
-		$exploredDirPath = $this->makeDirectory( $downloadDirPath, $target_key );
-
-		$this->Ftp->appendMes('downladed in: '.$downloadDirPath.'<br />');
-		$this->content.= 'downladed in: '.$downloadDirPath.'<br />';
-		if (! chdir($exploredDirPath) ) {
-			$this->_set_error_log('chdir error in: '.$exploredDirPath);
-			return false;//chdir error
-		}
-
-		// TODO ファイルNotFound対策
-		//$url = $this->_getDownloadUrl( $target_key, $downloadUrlFormat );
-		if (empty($downloadUrl)){
-			$this->_set_error_log('_getDownloadUrl false');
-			return false;
-		}
-
-		$downloadedFilePath = $this->_getDownloadFilePath( $realDirPath, $tempFilename );
+		$multiData = array(array(
+			'target_key' => $target_key,
+			'downloadUrl' => $downloadUrl,
+			'tempFilename' => $tempFilename,
+			'downloadedFilePath' => ''));
 		
-		// cache check
-		if ($cacheTTL && is_file($downloadedFilePath) && filemtime($downloadedFilePath) + $cacheTTL > $_SERVER['REQUEST_TIME']) {
+		if ($this->_multiDownloadFile( $multiData, $cacheTTL )) {
+			$downloadedFilePath = $multiData[0]['downloadedFilePath'];
 			return true;
-		}
-		
-		try {
-			try {
-				if(!function_exists('curl_init') ){
-					throw new Exception('curl_init function no found fail',1);
-				}
-			} catch (Exception $e) {
-				$this->_set_error_log($e->getMessage());
-				return false;
-			}
-
-			$ch = curl_init($downloadUrl);
-			if($ch === false ){
-				throw new Exception('curl_init fail',2);
-			}
-			$this->Ftp->appendMes('curl_init OK<br />');
-		} catch (Exception $e) {
-			$this->_set_error_log($e->getMessage());
+		} else {
 			return false;
 		}
-
-		$fp = fopen($downloadedFilePath, "w");
-
-		try {
-			$setopt1 = curl_setopt($ch, CURLOPT_FILE, $fp);
-			$setopt2 = curl_setopt($ch, CURLOPT_HEADER, 0);
-			$setopt3 = curl_setopt($ch, CURLOPT_FAILONERROR, true);
-
-			if(!$setopt1 || !$setopt2 || !$setopt3 ){
-				throw new Exception('curl_setopt fail',3);
-			}
-		} catch (Exception $e) {
-			$this->_set_error_log($e->getMessage());
-
-			fclose($fp);
-			return false;
-		}
-
-		//safe_mode  CURLOPT_FOLLOWLOCATION cannot be activated when in safe_mode
-		if (ini_get('safe_mode') != "1"){
-			try {
-				//redirect suport
-				$setopt4 = curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-				$setopt5 = curl_setopt($ch, CURLOPT_MAXREDIRS, 4);
-
-				if(!$setopt4 || !$setopt5 ){
-					throw new Exception('curl_setopt CURLOPT_FOLLOWLOCATION fail skip',4);
-				}
-			} catch (Exception $e) {
-				$this->_set_error_log($e->getMessage());
-			}
-		}
-
-		//SSL NO VERIFY setting
-		$URI_PARTS = parse_url($downloadUrl);
-		if (strtolower($URI_PARTS["scheme"]) == 'https' ){
-			try {
-				$setopt6 = curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-				$setopt7 = curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-				if(!$setopt6 || !$setopt7 ){
-					throw new Exception('curl_setopt SSL fail',5);
-				}
-			} catch (Exception $e) {
-				$this->_set_error_log($e->getMessage());
-
-				fclose($fp);
-				return false;
-			}
-		}
-
-		try {
-			try {
-				if(!function_exists('curl_exec') ){
-					throw new Exception('curl_exec function not found fail',6);
-				}
-			} catch (Exception $e) {
-				$this->_set_error_log($e->getMessage());
-				return false;
-			}
-
-			$result = curl_exec($ch);
-			if($result === false ){
-				throw new Exception('curl_exec fail:'.$downloadUrl.'<br />'.$downloadedFilePath,7);
-			}
-			$this->Ftp->appendMes('curl exec OK<br />');
-		} catch (Exception $e) {
-			$this->_set_error_log($e->getMessage());
-
-			fclose($fp);
-			return false;
-		}
-
-		fclose($fp);
-
-		return true;
 	}
 	
 	/**
