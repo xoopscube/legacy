f3d35771e543b63bd29ae01257ca495ab9fb82e5
Yoshi Sakai <yoshi.sakai@gmail.com>
2012-08-11 09:35:48 +0900

    About InstallerChecker
    Added if if clause.

--- html/modules/legacy/preload/InstallerChecker.class.php
+++ html/modules/legacy/preload/InstallerChecker.class.php
@@ -1,23 +1,23 @@
 <?php
-/**
- *
- * @package Legacy
- * @version $Id: InstallerChecker.class.php,v 1.5 2012/07/28 18:25:43 yoshis Exp $
- * @copyright Copyright 2005-2007 XOOPS Cube Project  <https://github.com/xoopscube/>
- * @license http://xoopscube.sourceforge.net/license/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
- *
- */
+    /**
+     *
+     * @package Legacy
+     * @version $Id: InstallerChecker.class.php,v 1.5 2012/08/11 09:32:20 yoshis Exp $
+     * @copyright Copyright 2005-2007 XOOPS Cube Project  <https://github.com/xoopscube/>
+     * @license http://xoopscube.sourceforge.net/license/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
+     *
+     */
 
-if (!defined('XOOPS_ROOT_PATH')) exit();
+    if (!defined('XOOPS_ROOT_PATH')) exit();
 
-if (!defined("LEGACY_INSTALLERCHECKER_ACTIVE"))
-    define("LEGACY_INSTALLERCHECKER_ACTIVE", true);
+    if (!defined("LEGACY_INSTALLERCHECKER_ACTIVE"))
+        define("LEGACY_INSTALLERCHECKER_ACTIVE", true);
 
-/**
- * This filter checks whether the install-wizard directory is removed.
- * If it is not removed yet, this filter warns to remove the install-wizard
- * directory.
- */
+    /**
+     * This filter checks whether the install-wizard directory is removed.
+     * If it is not removed yet, this filter warns to remove the install-wizard
+     * directory.
+     */
 class Legacy_InstallerChecker extends XCube_ActionFilter
 {
     // Remove install folder.
@@ -69,24 +69,26 @@ class Legacy_InstallerChecker extends XCube_ActionFilter
 
     function preBlockFilter()
     {
-        $this->remove_install_folder(XOOPS_ROOT_PATH . "/install");
-        $this->rename_install_folder(XOOPS_ROOT_PATH . "/install");
         if (LEGACY_INSTALLERCHECKER_ACTIVE == true && is_dir(XOOPS_ROOT_PATH . "/install")) {
-            $root =& XCube_Root::getSingleton();
-            $root->mLanguageManager->loadModuleMessageCatalog('legacy');
-            $xoopsConfig = $root->mContext->mXoopsConfig;
-            require_once XOOPS_ROOT_PATH . '/class/template.php';
-            $xoopsTpl = new XoopsTpl();
-            $xoopsTpl->assign(array('xoops_sitename' => htmlspecialchars($xoopsConfig['sitename']),
-                'xoops_themecss' => xoops_getcss(),
-                'xoops_imageurl' => XOOPS_THEME_URL . '/' . $xoopsConfig['theme_set'] . '/',
-                'lang_message_confirm' => XCube_Utils::formatMessage(_MD_LEGACY_MESSAGE_INSTALL_COMPLETE_CONFIRM, XOOPS_ROOT_PATH . "/install"),
-                'lang_message_warning' => XCube_Utils::formatMessage(_MD_LEGACY_MESSAGE_INSTALL_COMPLETE_WARNING, XOOPS_ROOT_PATH . "/install")
-            ));
-            $xoopsTpl->compile_check = true;
-            // @todo filebase template with absolute file path
-            $xoopsTpl->display(XOOPS_ROOT_PATH . '/modules/legacy/templates/legacy_install_completed.html');
-            exit();
+            $this->remove_install_folder(XOOPS_ROOT_PATH . "/install");
+            $this->rename_install_folder(XOOPS_ROOT_PATH . "/install");
+            if (is_dir(XOOPS_ROOT_PATH . "/install")) {
+                $root =& XCube_Root::getSingleton();
+                $root->mLanguageManager->loadModuleMessageCatalog('legacy');
+                $xoopsConfig = $root->mContext->mXoopsConfig;
+                require_once XOOPS_ROOT_PATH . '/class/template.php';
+                $xoopsTpl = new XoopsTpl();
+                $xoopsTpl->assign(array('xoops_sitename' => htmlspecialchars($xoopsConfig['sitename']),
+                    'xoops_themecss' => xoops_getcss(),
+                    'xoops_imageurl' => XOOPS_THEME_URL . '/' . $xoopsConfig['theme_set'] . '/',
+                    'lang_message_confirm' => XCube_Utils::formatMessage(_MD_LEGACY_MESSAGE_INSTALL_COMPLETE_CONFIRM, XOOPS_ROOT_PATH . "/install"),
+                    'lang_message_warning' => XCube_Utils::formatMessage(_MD_LEGACY_MESSAGE_INSTALL_COMPLETE_WARNING, XOOPS_ROOT_PATH . "/install")
+                ));
+                $xoopsTpl->compile_check = true;
+                // @todo filebase template with absolute file path
+                $xoopsTpl->display(XOOPS_ROOT_PATH . '/modules/legacy/templates/legacy_install_completed.html');
+                exit();
+            }
         }
     }
 }
