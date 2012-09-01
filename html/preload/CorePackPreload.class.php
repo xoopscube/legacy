4a2758ac3a9df4abca568179527753f11950c365
nao-pon <hypweb@gmail.com>
2012-09-01 23:17:04 +0900

    CorePack:[CorePackPreload] set smarty plugin dir & compile_id
    ref. https://www.facebook.com/groups/XoopsX/permalink/501751083186096/

--- html/preload/CorePackPreload.class.php
+++ html/preload/CorePackPreload.class.php
@@ -5,5 +5,29 @@ include_once(XOOPS_ROOT_PATH . '/include/corepack_version.php');
 
 class CorePackPreload extends XCube_ActionFilter
 {
+	function preBlockFilter()
+	{
+		$this->mRoot->mDelegateManager->add( 'XoopsTpl.New' , array( $this , 'tplhook' ) , XCUBE_DELEGATE_PRIORITY_6 ) ;
+	}
 
+	function tplhook( &$xoopsTpl )
+	{
+		if (! defined('HYP_COMMON_PRELOAD_CONF')) {
+			$xoopsConfig = $this->mRoot->mContext->mXoopsConfig;
+
+			$target_dir = XOOPS_TRUST_PATH.'/libs/smartyplugins';
+			if(is_dir($target_dir)) {
+				$_1st = array_shift($xoopsTpl->plugins_dir);
+				if ($_1st === $target_dir) {
+					$_1st = array_shift($xoopsTpl->plugins_dir);
+				}
+				// regist 2nd
+				array_unshift($xoopsTpl->plugins_dir, $_1st, $target_dir);
+			}
+
+			$compile_id = substr(XOOPS_URL, 7) . '-' . $xoopsConfig['template_set'] . '-' . $xoopsConfig['theme_set'] ;
+			$xoopsTpl->compile_id = $compile_id ;
+			$xoopsTpl->_compile_id = $compile_id ;
+		}
+	}
 }
