327535ff7a4d229752e3cf3ba1f38725d4127f75
nao-pon <hypweb@gmail.com>
2012-09-11 22:43:42 +0900

    fix getRedirectUrl()
    セーフモードでリダイレクト先のURLが正しく取得できないことがある問題の修正

--- xoops_trust_path/modules/xupdate/class/XupdateUtils.class.php
+++ xoops_trust_path/modules/xupdate/class/XupdateUtils.class.php
@@ -107,13 +107,18 @@ class Xupdate_Utils
 	 * @param int $limit
 	 * @return string
 	 */
-	public static function getRedirectUrl($url, $limit = 4) {
+	public static function getRedirectUrl($url) {
 		$headers = get_headers($url, 1);
-		if($limit &&  preg_match('#^HTTP/\d\.\d\s+(301|302|303|307)#',$headers[0]) && isset($headers['Location'])) {
-			return self::getRedirectUrl(trim($headers['Location']), --$limit);
+		$location = isset($headers['Location'])? $headers['Location'] : (isset($headers['location'])? $headers['location'] : '');
+		if ($location) {
+			if (is_array($location)) {
+				$url = array_pop($location);
+			} else {
+				$url = $location;
+			}
 		}
 		return $url;
-    }
+	}
     
     /**
      * Check, Is directory writable
