9c5b4d695b1d8c2f1dc244829a0ce0431683580d
nao-pon <hypweb@gmail.com>
2012-12-29 16:07:57 +0900

    fix for (safe_mode or open_basedir) && !allow_url_fopen
    PHP がセーフモードもしくは open_basedir が設定されている、且つ allow_url_fopen が許可されていない環境でアーカイブの取得ができない問題を修正。

--- xoops_trust_path/modules/xupdate/class/XupdateUtils.class.php
+++ xoops_trust_path/modules/xupdate/class/XupdateUtils.class.php
@@ -107,19 +107,58 @@ class Xupdate_Utils
 	 * @param int $limit
 	 * @return string
 	 */
-	public static function getRedirectUrl($url) {
-		$headers = get_headers($url, 1);
-		$location = isset($headers['Location'])? $headers['Location'] : (isset($headers['location'])? $headers['location'] : '');
-		if ($location) {
-			if (is_array($location)) {
-				$url = array_pop($location);
-			} else {
-				$url = $location;
+	public static function getRedirectUrl($url, $redirect = 10) {
+		if ($headers = @ get_headers($url, 1)) {
+			$location = isset($headers['Location'])? $headers['Location'] : (isset($headers['location'])? $headers['location'] : '');
+			if ($location) {
+				if (is_array($location)) {
+					$url = array_pop($location);
+				} else {
+					$url = $location;
+				}
 			}
+		} else {
+			$url = self::curlGetRedirectUrl($url);
 		}
 		return $url;
 	}
     
+	/**
+	 * Get redirect URL with cURL
+	 * 
+	 * @param $url
+	 * @param $ch
+	 * @param $max_redirect
+	 * @param $redirects
+	 * @return string
+	 */
+	public static function curlGetRedirectUrl($url, $ch = null, $max_redirect = 10, $redirects = 0) {
+		if ($max_redirect < $redirects) {
+			return $url;
+		}
+		if (! $ch) {
+			$ch = curl_init($url);
+		}
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		
+		$data = curl_exec($ch);
+		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($http_code == 301 || $http_code == 302 || $http_code == 303 || $http_code == 307) {
+			list($header) = explode("\r\n\r\n", $data, 2);
+			if (preg_match('/(?:Location:|URI:)(.*?)\n/i', $header, $matches)) {
+				$url = trim($matches[1]);
+				curl_setopt($ch, CURLOPT_URL, $url);
+				$redirects++;
+				return self::curlGetRedirectUrl($url, $ch, $max_redirect, $redirects);
+			}
+		}
+		curl_close($ch);
+		return $url;
+	}
+	
     /**
      * Check, Is directory writable
      * 
