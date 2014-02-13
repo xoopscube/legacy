60cbb8d4a1e6ee09cdcffb221f3fedd4cc5930a5
nao-pon <hypweb@gmail.com>
2014-02-13 17:47:23 +0900

    [CorePack] pilot version 20140208.140213

--- html/include/corepack_version.php
+++ html/include/corepack_version.php
@@ -1,5 +1,5 @@
 <?php
 // XOOPS X (ten) CorePack version
-define('XOOPSX_COREPACK_VERSION', '20140208.140211');
+define('XOOPSX_COREPACK_VERSION', '20140208.140213');
 // detaild_version for X-update
 define('_MI_LEGACY_DETAILED_VERSION', 'CorePack ' . XOOPSX_COREPACK_VERSION);
