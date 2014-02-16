f3e274a5d4c41e7d7cd79beb73253479908760ca
nao-pon <hypweb@gmail.com>
2014-02-16 16:45:27 +0900

    [install] fix xoopscube/legacy#97
    fix problem of invalid timezone ID on installer

--- html/install/wizards/install_siteInit.inc.php
+++ html/install/wizards/install_siteInit.inc.php
@@ -15,6 +15,40 @@ $current_timediff = floatval(substr($current_timezone,0,1).(substr($current_time
 $wizard->assign('current_timediff', $current_timediff);
 
 $wizard->assign('timediffs', XoopsLists::getTimeZoneList());
+$wizard->assign('timezones', array (
+	'-12'  => 'Kwajalein',
+	'-11'  => 'Pacific/Midway',
+	'-10'  => 'Pacific/Honolulu',
+	'-9'   => 'America/Adak',
+	'-8'   => 'America/Anchorage',
+	'-7'   => 'America/Los_Angeles',
+	'-6'   => 'America/Denver',
+	'-5'   => 'America/Guayaquil',
+	'-4.5' => 'America/Caracas',
+	'-4'   => 'America/New_York',
+	'-3'   => 'America/Halifax',
+	'-2'   => 'Atlantic/South_Georgia',
+	'-1'   => 'Atlantic/Cape_Verde',
+	'0'    => 'Atlantic/Azores',
+	'1'    => 'Europe/Dublin',
+	'2'    => 'Europe/Belgrade',
+	'3'    => 'Asia/Kuwait',
+	'3.5'  => 'Asia/Tehran',
+	'4'    => 'Asia/Muscat',
+	'4.5'  => 'Asia/Kabul',
+	'5'    => 'Asia/Ashgabat',
+	'5.5'  => 'Asia/Kolkata',
+	'5.75' => 'Asia/Kathmandu',
+	'6'    => 'Asia/Dhaka',
+	'6.5'  => 'Asia/Rangoon',
+	'7'    => 'Asia/Jakarta',
+	'8'    => 'Asia/Krasnoyarsk',
+	'9'    => 'Asia/Tokyo',
+	'9.5'  => 'Australia/Darwin',
+	'10'   => 'Asia/Yakutsk',
+	'11'   => 'Australia/Canberra',
+	'12'   => 'Pacific/Fiji',
+	'13'   => 'Pacific/Tongatapu'));
 
 $wizard->render('install_siteInit.tpl.php');
 ?>
