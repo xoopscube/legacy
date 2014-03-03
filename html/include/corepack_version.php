86fce136dc8ed374016d58cec34e303040f9f27e
nao-pon <hypweb@gmail.com>
2014-03-03 21:23:30 +0900

    [CorePack] version 20140303

--- html/include/corepack_version.php
+++ html/include/corepack_version.php
@@ -1,5 +1,5 @@
 <?php
 // XOOPS X (ten) CorePack version
-define('XOOPSX_COREPACK_VERSION', '20140228');
+define('XOOPSX_COREPACK_VERSION', '20140303');
 // detaild_version for X-update
 define('_MI_LEGACY_DETAILED_VERSION', 'CorePack ' . XOOPSX_COREPACK_VERSION);
