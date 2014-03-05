f7ccb2b21274dfdbff012e2ab7d3c442986178a5
nao-pon <hypweb@gmail.com>
2014-03-05 14:59:40 +0900

    fix parse ini option 'force_languages'

--- xoops_trust_path/modules/xupdate/include/ModulesIniDadaSet.class.php
+++ xoops_trust_path/modules/xupdate/include/ModulesIniDadaSet.class.php
@@ -476,7 +476,11 @@ class Xupdate_ModulesIniDadaSet
 			$item_arr['modinfo'] = $item['modinfo'] ;
 		}
 		if(isset($item['force_languages'])){
-			$item_arr['force_languages'] = array_map('trim', explode(',', trim($item['force_languages'])));
+			if (is_array($item['force_languages'])) {
+				$item_arr['force_languages'] = $item['force_languages'];
+			} else {
+				$item_arr['force_languages'] = array_map('trim', explode(',', trim($item['force_languages'])));
+			}
 		} else {
 			$item_arr['force_languages'] = array();
 		}
