7fa82842063c6e1a397e93d4e59603eedc931fe8
nao-pon <hypweb@gmail.com>
2014-05-05 11:56:33 +0900

    [pulot] version 20140505.140505

--- html/include/corepack_version.php
+++ html/include/corepack_version.php
@@ -1,5 +1,5 @@
 <?php
 // XOOPS X (ten) CorePack version
-define('XOOPSX_COREPACK_VERSION', '20140505');
+define('XOOPSX_COREPACK_VERSION', '20140505.140505');
 // detaild_version for X-update
 define('_MI_LEGACY_DETAILED_VERSION', 'CorePack ' . XOOPSX_COREPACK_VERSION);
