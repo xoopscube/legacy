88eef6582d4fdeee201b40b2728f2d6c1e66113c
nao-pon <hypweb@gmail.com>
2015-05-12 21:13:48 +0900

    Revert "use XOOPS_DB_TYPE 'mysqli' when not support 'mysql'"
    This reverts commit 2bd77e9e28ebabe43dd1863ad218611e0d945b86.

--- html/mainfile.dist.php
+++ html/mainfile.dist.php
@@ -45,11 +45,7 @@ if ( !defined("XOOPS_MAINFILE_INCLUDED") ) {
 
     // Database
     // Choose the database to be used
-    if (extension_loaded('mysql')) {
-        define('XOOPS_DB_TYPE', 'mysql');
-    } else {
-        define('XOOPS_DB_TYPE', 'mysqli');
-    }
+    define('XOOPS_DB_TYPE', 'mysql');
 
     // Table Prefix
     // This prefix will be added to all new tables created to avoid name conflict in the database. If you are unsure, just use the default 'xoops'.
