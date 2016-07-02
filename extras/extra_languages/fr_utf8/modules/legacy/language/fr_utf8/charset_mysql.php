46b76be5bde61b970e4b9fadc20c376c4769e835
Yoshihiro OKUMURA <okumura@brain.riken.jp>
2016-07-02 21:00:13 +0900

    fixed mysql character set client handshake issues. - use function_exists() to detect 'mysqli_set_charset' availability. - remove LEGACY_JAPANESE_ANTI_CHARSETMYSQL check if not Japanese resources.

--- extras/extra_languages/fr_utf8/modules/legacy/language/fr_utf8/charset_mysql.php
+++ extras/extra_languages/fr_utf8/modules/legacy/language/fr_utf8/charset_mysql.php
@@ -2,7 +2,5 @@
 if (!defined('XOOPS_ROOT_PATH')) {
     exit();
 }
-if (!defined("LEGACY_JAPANESE_ANTI_CHARSETMYSQL")) {
-    $GLOBALS['xoopsDB']->queryF("/*!40101 SET NAMES utf8 */");
-    $GLOBALS['xoopsDB']->queryF("/*!40101 SET SESSION collation_connection=utf8 */");
-}
+$GLOBALS['xoopsDB']->queryF("/*!40101 SET NAMES utf8 */");
+$GLOBALS['xoopsDB']->queryF("/*!40101 SET SESSION collation_connection=utf8_general_ci */");
