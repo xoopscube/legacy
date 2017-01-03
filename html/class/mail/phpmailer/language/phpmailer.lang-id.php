4f7ca97d97adb243e75f051398583a27ce911e11
nao-pon <hypweb@gmail.com>
2017-01-03 10:41:03 +0900

    fix #121 PHPMailer < 5.2.20 has Remote Code Execution
    PHPMailer update to 5.2.21

--- html/class/mail/phpmailer/language/phpmailer.lang-id.php
+++ html/class/mail/phpmailer/language/phpmailer.lang-id.php
@@ -24,4 +24,3 @@ $PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Connect() gagal.';
 $PHPMAILER_LANG['smtp_error']           = 'Kesalahan peladen SMTP : ';
 $PHPMAILER_LANG['variable_set']         = 'Tidak berhasil mengatur atau mengatur ulang variable : ';
 //$PHPMAILER_LANG['extension_missing']    = 'Extension missing: ';
-
--- html/class/mail/phpmailer/language/phpmailer.lang-ms.php
+++ html/class/mail/phpmailer/language/phpmailer.lang-ms.php
@@ -15,7 +15,7 @@ $PHPMAILER_LANG['file_access']          = 'Tidak dapat mengakses fail: ';
 $PHPMAILER_LANG['file_open']            = 'Ralat Fail: Tidak dapat membuka fail: ';
 $PHPMAILER_LANG['from_failed']          = 'Berikut merupakan ralat dari alamat e-mel: ';
 $PHPMAILER_LANG['instantiate']          = 'Tidak dapat memberi contoh fungsi e-mel.';
-$PHPMAILER_LANG['invalid_address']      = 'Alamat emel tidak sah';
+$PHPMAILER_LANG['invalid_address']      = 'Alamat emel tidak sah: ';
 $PHPMAILER_LANG['mailer_not_supported'] = ' jenis penghantar emel tidak disokong.';
 $PHPMAILER_LANG['provide_address']      = 'Anda perlu menyediakan sekurang-kurangnya satu alamat e-mel penerima.';
 $PHPMAILER_LANG['recipients_failed']    = 'Ralat SMTP: Penerima e-mel berikut telah gagal: ';
@@ -24,4 +24,3 @@ $PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Connect() telah gagal.';
 $PHPMAILER_LANG['smtp_error']           = 'Ralat pada pelayan SMTP: ';
 $PHPMAILER_LANG['variable_set']         = 'Tidak boleh menetapkan atau menetapkan semula pembolehubah: ';
 //$PHPMAILER_LANG['extension_missing']    = 'Extension missing: ';
-
