c46fe0c4d6f3ffa0e46c70257d2e3a5491851a02
Kujiy <kujiy@users.noreply.github.com>
2018-03-14 17:41:11 +0900

    Show the error message when unable to connect to the database (#125)
    * [master] databaseに接続できない場合にメンテ案内文を表示
    
    * [master] databaseに接続できない場合は英文のエラーメッセージを表示 & Http status 503 を出力

--- html/class/database/databasefactory.php
+++ html/class/database/databasefactory.php
@@ -1,17 +1,16 @@
 <?php
 class XoopsDatabaseFactory
 {
-
     public function XoopsDatabaseFactory()
     {
     }
 
     /**
      * Get a reference to the only instance of database class and connects to DB
-     * 
-     * if the class has not been instantiated yet, this will also take 
+     *
+     * if the class has not been instantiated yet, this will also take
      * care of that
-     * 
+     *
      * @static
      * @staticvar   object  The only instance of database class
      * @return      object  Reference to the only instance of database class
@@ -34,6 +33,8 @@ class XoopsDatabaseFactory
             $instance->setLogger(XoopsLogger::instance());
             $instance->setPrefix(XOOPS_DB_PREFIX);
             if (!$instance->connect()) {
+                echo "Service temporarily unavailable. (Unable to connect to the database)";
+                header('HTTP/1.1 503 Service Temporarily Unavailable');
                 trigger_error("Unable to connect to database", E_USER_ERROR);
             }
         }
@@ -43,7 +44,7 @@ class XoopsDatabaseFactory
     /**
      * Gets a reference to the only instance of database class. Currently
      * only being used within the installer.
-     * 
+     *
      * @static
      * @staticvar   object  The only instance of database class
      * @return      object  Reference to the only instance of database class
