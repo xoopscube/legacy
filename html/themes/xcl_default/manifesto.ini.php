2ec2b3c58a5f7581f307023a7738e24a5383e9ab
Info Opensource <info.opensource@gmail.com>
2019-02-06 22:28:01 +0100

    Theme rough draft
    XCL default outline

---
+++ html/themes/xcl_default/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="Bootstrap 4 XCL Default"
+Depends=Legacy_RenderSystem,legacy
+Url="https://xoopscube.github.io/"
+Version="2.3.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XCL HTML5 Bootstrap JS"
+Author=Nuno Luciano
+ScreenShot="screenshot.png"
+Description="Alpha rough draft ! XCL Frontend Bootstrap 4. Left sidebar, center-block, content, centerleft and center-right block."
+W3C=YES
+
+Licence="MIT"
+
+*/
+?>
