c87ff9f3a76ad4a1c16761464c666f1d65e632c3
gigamaster <1905497+gigamaster@users.noreply.github.com>
2022-12-20 16:47:09 +0100

    Default Theme v2.3.1 built with PicoCSS v1.5.6

--- html/themes/xcl_default/manifesto.ini.php
+++ html/themes/xcl_default/manifesto.ini.php
@@ -6,14 +6,14 @@ Name="Default Theme"
 Depends=Legacy_RenderSystem,legacy
 ThemeOptions="index.php"
 Url="https://github.com/xoopscube/legacy"
-Version="1.5.6"
+Version="2.3.1"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
 Format="XOOPS2 Legacy Style"
 Author=Nuno Luciano @gigamaster
 ScreenShot="default-picocss-screenshot.png"
-Description="Default theme with a classic company or blog layout with a sidebar. Built with Pico.css"
+Description="Default theme with a classic company or blog layout with a sidebar. Built with Pico.css v1.5.6"
 W3C=NG
 
 License="MIT"
