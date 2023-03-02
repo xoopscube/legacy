15146c6a938373673b7e3b037a072ca6243cf6fd
gigamaster <1905497+gigamaster@users.noreply.github.com>
2023-03-03 00:37:30 +0100

    XCL 2023 Admin Notification

--- html/admin.php
+++ html/admin.php
@@ -7,7 +7,7 @@
  */
 /*------------------------------------------------------------------------*
  |  This file was entirely rewritten by the XOOPS Cube Legacy project for |
- |   keeping compatibility with XOOPS 2.0.x <https://www.xoops.org>       |
+ |   keeping compatibility with XOOPS 2.0.x                               |
  *------------------------------------------------------------------------*/
 
 include 'mainfile.php';
@@ -17,20 +17,27 @@ class DefaultSystemCheckFunction
     public static function DefaultCheck()
     {
         if (!defined('LEGACY_INSTALLERCHECKER_ACTIVE')) {
-            define('LEGACY_INSTALLERCHECKER_ACTIVE', true);
+            define('LEGACY_INSTALLERCHECKER_ACTIVE', true); $var = [];
         }
-// if ( LEGACY_INSTALLERCHECKER_ACTIVE == true || XUPDATE_INSTALLERCHECKER_ACTIVE == true && is_dir(XOOPS_ROOT_PATH . '/install' ) ) {
+        $alert = [];
+        $alert[]= '<div class="alert-notify">';
+        // if ( LEGACY_INSTALLERCHECKER_ACTIVE == true || XUPDATE_INSTALLERCHECKER_ACTIVE == true && is_dir(XOOPS_ROOT_PATH . '/install' ) ) {
         //if (LEGACY_INSTALLERCHECKER_ACTIVE == true && XUPDATE_INSTALLERCHECKER_ACTIVE == true) {
             //if ( LEGACY_INSTALLERCHECKER_ACTIVE == true && is_dir(XOOPS_ROOT_PATH . '/install' ) ) {
             if (is_dir(XOOPS_ROOT_PATH . '/install/')) {
-                //xoops_error( writeLines(sprintf(_WARNINSTALL2, XOOPS_ROOT_PATH.'/install/'), 'install', 'warning'));
-                xoops_error((XCube_Utils::formatString(nl2Br(_WARNINSTALL2), XOOPS_ROOT_PATH . '/install')));
+                //xoops_error(XCube_Utils::formatString( _WARNINSTALL2, XOOPS_ROOT_PATH . '/install'),'install', 'warning'); version XCL 2.3
+                $alert[]= '<div class="error"><button type="button" class="button alert-close" data-dismiss="alert" aria-hidden="true">&times;</button>'. XCube_Utils::formatString( _WARNINSTALL2, XOOPS_ROOT_PATH . '/install'). '</div>';
             }
             if (is_writable(XOOPS_ROOT_PATH . '/mainfile.php')) {
-                //xoops_error(sprintf(_WARNINWRITEABLE, XOOPS_ROOT_PATH.'/mainfile.php'), '', 'warning');
-                xoops_error(XCube_Utils::formatString(_WARNINWRITEABLE, XOOPS_ROOT_PATH . '/mainfile.php'));
+                //xoops_error(sprintf(_WARNINWRITEABLE, XOOPS_ROOT_PATH.'/mainfile.php'), '', 'warning'); version Legacy 2.2
+                //xoops_error(XCube_Utils::formatString(_WARNINWRITEABLE, XOOPS_ROOT_PATH . '/mainfile.php')); version XCL 2.3
+                $alert[]= '<div class="error"><button type="button" class="button alert-close" data-dismiss="alert" aria-hidden="true">&times;</button>'. XCube_Utils::formatString(_WARNINWRITEABLE, XOOPS_ROOT_PATH . '/mainfile.php'). '</div>';
             }
-       // }
+        $alert[]= '</div>';
+        foreach ($alert as $notify) {
+            echo "$notify ";
+        }
+
     }
 }
 
