dccdf35d338835fdc9743fba5cd9cbba49f6a4ca
nunoluciano <nunoluciano@yahoo.com>
2023-04-19 06:01:29 +0200

    Update translations

--- xoops_trust_path/modules/xelfinder/language/fr_utf8/main.php
+++ xoops_trust_path/modules/xelfinder/language/fr_utf8/main.php
@@ -16,9 +16,9 @@ if ( defined( 'FOR_XOOPS_LANG_CHECKER' ) || ! defined( $constpref . '_LOADED_MAI
 	define( $constpref . '_FINDER_DESC', 'Permet aux membres de télécharger et de gérer des documents et des images.' );
 	define( $constpref . '_OPEN_MANAGER', 'Ouvrir le gestionnaire de fichiers' );
 	define( $constpref . '_OPEN_WINDOW', 'Fenêtre contextuelle' );
-	define( $constpref . '_OPEN_FULL', 'New window' );
-	define( $constpref . '_OPEN_WINDOW_ADMIN', 'Pop window (Admin mode)' );
-	define( $constpref . '_OPEN_FULL_ADMIN', 'New window (Admin mode)' );
-	define( $constpref . '_ADMIN_PANEL', 'Go to admin panel' );
+	define( $constpref . '_OPEN_FULL', 'Nouvelle fenêtre' );
+	define( $constpref . '_OPEN_WINDOW_ADMIN', 'Fenêtre contextuelle (mode administrateur)' );
+	define( $constpref . '_OPEN_FULL_ADMIN', 'Nouvelle fenêtre (mode administrateur)' );
+	define( $constpref . '_ADMIN_PANEL', 'Aller au panneau de contrôle' );
 
 }
