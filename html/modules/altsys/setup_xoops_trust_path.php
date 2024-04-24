bb46a38e0b58767e8896413ed481524ddbb0b045
gigamaster <1905497+gigamaster@users.noreply.github.com>
2024-04-25 00:08:33 +0200

    update altsys xcl v2.4.0

--- html/modules/altsys/setup_xoops_trust_path.php
+++ html/modules/altsys/setup_xoops_trust_path.php
@@ -5,7 +5,7 @@
  * @version    2.3.1
  * @author     Other authors gigamaster, 2020 XCL/PHP7
  * @author     Gijoe (Peak)
- * @copyright  (c) 2005-2023 Gijoe (https://peak.ne.jp/)
+ * @copyright  (c) 2005-2024 Gijoe (https://peak.ne.jp/)
  * @license    GPL v2.0
  */
 
