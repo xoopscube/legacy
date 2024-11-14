c826d2927e26b09b06fc0c222d61395443e1cfbb
Ryuji AMANO <ryuji@ryus.co.jp>
2024-11-14 12:04:00 +0900

    fix: Archive_Zip.php. Change each to foreach

--- html/class/Archive_Zip.php
+++ html/class/Archive_Zip.php
@@ -686,31 +686,27 @@ class Archive_Zip
     * @param bool $p_with_code If true, gives the name and the int value.
     * @return string The error name.
     */
-    public function errorName($p_with_code=false)
+    public function errorName($p_with_code = false)
     {
         $v_const_list = get_defined_constants();
-    
-          // ----- Extract error constants from all const.
-        for (reset($v_const_list);
-             list($v_key, $v_value) = each($v_const_list);) {
-            if ('ARCHIVE_ZIP_ERR_'
-                == substr($v_key, 0, strlen('ARCHIVE_ZIP_ERR_'))) {
+
+        // ----- Extract error constants from all const.
+        $v_error_list = [];
+        foreach ($v_const_list as $v_key => $v_value) {
+            if (substr($v_key, 0, strlen('ARCHIVE_ZIP_ERR_')) === 'ARCHIVE_ZIP_ERR_') {
                 $v_error_list[$v_key] = $v_value;
             }
         }
-    
-        // ----- Search the name form the code value
-        $v_key=array_search($this->_error_code, $v_error_list, true);
-        if (false != $v_key) {
-            $v_value = $v_key;
-        } else {
-            $v_value = 'NoName';
-        }
-    
+
+        // ----- Search the name from the code value
+        $v_key = array_search($this->_error_code, $v_error_list, true);
+        $v_value = $v_key !== false ? $v_key : 'NoName';
+
+        // ----- Return the error name with or without the error code
         if ($p_with_code) {
-            return($v_value.' ('.$this->_error_code.')');
+            return $v_value . ' (' . $this->_error_code . ')';
         } else {
-            return($v_value);
+            return $v_value;
         }
     }
     // }}}
@@ -914,7 +910,7 @@ class Archive_Zip
       }
 
     // ----- Swap the file descriptor
-    // Here is a trick : I swap the temporary fd with the zip fd, in order to 
+    // Here is a trick : I swap the temporary fd with the zip fd, in order to
     // use the following methods on the temporary fil and not the real archive
     $v_swap = $this->_zip_fd;
       $this->_zip_fd = $v_zip_temp_fd;
@@ -1419,7 +1415,7 @@ class Archive_Zip
             $this->_errorLog(ARCHIVE_ZIP_ERR_READ_OPEN_FAIL, "Unable to open file '$p_filename' in binary read mode");
             return Archive_Zip::errorCode();
         }
-        
+
           if ($p_params['no_compression']) {
               // ----- Read the file content
           $v_content_compressed = @fread($v_file, $p_header['size']);
@@ -3224,14 +3220,14 @@ class Archive_Zip
      */
   public function _check_parameters(&$p_params, $p_default)
   {
-    
+
     // ----- Check that param is an array
     if (!is_array($p_params)) {
         $this->_errorLog(ARCHIVE_ZIP_ERR_INVALID_PARAMETER,
                          'Unsupported parameter, waiting for an array');
         return Archive_Zip::errorCode();
     }
-    
+
     // ----- Check that all the params are valid
     for (reset($p_params); list($v_key, $v_value) = each($p_params);) {
         if (!isset($p_default[$v_key])) {
@@ -3248,7 +3244,7 @@ class Archive_Zip
             $p_params[$v_key] = $p_default[$v_key];
         }
     }
-    
+
     // ----- Check specific parameters
     $v_callback_list = [
         'callback_pre_add', 'callback_post_add',
